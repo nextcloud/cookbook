fb7d4efe90a6b2da477005d3934544900427d8f0
mrzapp <contact@jeppezapp.com>
2019-03-17 10:37:04 +0100

    editor styling

--- templates/content/edit.php
+++ templates/content/edit.php
@@ -1,50 +1,59 @@
 <?php $recipe = json_decode($_['current_node']->getContent(), true); ?>
 
 <form action="/index.php/apps/cookbook/update" method="POST">
-    <header>
-        <?php if(isset($recipe['image']) && $recipe['image']) { ?>
-            <input type="hidden" name="image" value="<?php echo $recipe['image']; ?>">
+    <fieldset>
+        <label>Name</label>
+        <input required type="text" name="name" value="<?php echo $recipe['name']; ?>"></h2>
+    </fieldset>
 
-            <figure>
-                <img src="/index.php/apps/cookbook/image?recipe=<?php echo $_['current_node']->getId(); ?>&size=full">
-            </figure>
-        <?php } ?>
+    <fieldset>
+        <label>Image URL</label>
+        <input type="text" name="image" value="<?php if(isset($recipe['image'])) { echo $recipe['image']; } ?>">
+    </fieldset>
 
-        <h2><input type="text" name="name" value="<?php echo $recipe['name']; ?>"></h2>
-        
-        <p><input type="number" name="recipeYield" value="<?php echo $recipe['recipeYield']; ?>"> serving<?php if($recipe['recipeYield'] > 1) { echo 's'; } ?></p>
-        
-        <?php 
-
-        $daily_dozen = [
-            'beans' => [ 'icon' => '🥛', 'name' => 'Beans and legumes' ],
-            'berries' => [ 'icon' => '🍓', 'name' => 'Berries' ],
-            'cruficerous_vegetables' => [ 'icon' => '🥦', 'name' => 'Cruciferous vegetables' ],
-            'flaxseeds' => [ 'icon' => '🌱', 'name' => 'Flaxseeds' ],
-            'greens' => [ 'icon' => '🥬', 'name' => 'Greens' ],
-            'nuts' => [ 'icon' => '🌰', 'name' => 'Nuts and seeds' ],
-            'other_fruits' => [ 'icon' => '🍌', 'name' => 'Other fruits' ],
-            'other_vegetables' => [ 'icon' => '🥑', 'name' => 'Other vegetables' ],
-            'spices' => [ 'icon' => '🌿', 'name' => 'Herbs and spices' ],
-            'whole_grains' => [ 'icon' => '🍞', 'name' => 'Whole grains' ],
-        ];
+    <fieldset>
+        <label>Servings</label>
+        <input type="number" name="recipeYield" value="<?php if(isset($recipe['recipeYield'])) { echo $recipe['recipeYield']; } ?>">
+    </fieldset>
     
-        ?>
+    <?php 
 
-        <?php foreach($daily_dozen as $id => $ingredient) { ?>
-            <?php $has_ingredient = strpos($recipe['dailyDozen'], $id) !== false; ?>
-            
-            <label title="<?php echo $ingredient['name']; ?>">
-                <?php echo $ingredient['icon']; ?>
-                <input type="checkbox" name="dailyDozen[<?php echo $id; ?>]" <?php if($has_ingredient) { echo 'checked'; } ?>>
-            </label>
-        <?php } ?>
-    </header>
+    $daily_dozen = [
+        'beans' => [ 'icon' => '🥛', 'name' => 'Beans and legumes' ],
+        'berries' => [ 'icon' => '🍓', 'name' => 'Berries' ],
+        'cruficerous_vegetables' => [ 'icon' => '🥦', 'name' => 'Cruciferous vegetables' ],
+        'flaxseeds' => [ 'icon' => '🌱', 'name' => 'Flaxseeds' ],
+        'greens' => [ 'icon' => '🥬', 'name' => 'Greens' ],
+        'nuts' => [ 'icon' => '🌰', 'name' => 'Nuts and seeds' ],
+        'other_fruits' => [ 'icon' => '🍌', 'name' => 'Other fruits' ],
+        'other_vegetables' => [ 'icon' => '🥑', 'name' => 'Other vegetables' ],
+        'spices' => [ 'icon' => '🌿', 'name' => 'Herbs and spices' ],
+        'whole_grains' => [ 'icon' => '🍞', 'name' => 'Whole grains' ],
+    ];
 
-    <aside>
+    ?>
+
+    <fieldset>
+        <label>Daily dozen</label>
+
         <ul>
-            <h3>Ingredients</h3>
+            <?php foreach($daily_dozen as $id => $ingredient) { ?>
+                <?php $has_ingredient = strpos($recipe['dailyDozen'], $id) !== false; ?>
 
+                <li>    
+                    <label title="<?php echo $ingredient['name']; ?>">
+                        <?php echo $ingredient['name'] . ' ' . $ingredient['icon']; ?>
+                        <input type="checkbox" name="dailyDozen[<?php echo $id; ?>]" <?php if($has_ingredient) { echo 'checked'; } ?>>
+                    </label>
+                </li>
+            <?php } ?>
+        </ul>
+    </fieldset>
+
+    <fieldset>
+        <label>Ingredients</label>
+
+        <ul>
             <?php foreach($recipe['recipeIngredient'] as $i => $ingredient) {  ?>
                 <li>
                     <input type="text" name="recipeIngredient[]" value="<?php echo $ingredient; ?>">
@@ -53,12 +62,12 @@
             <?php } ?>
             <button class="icon-add"></button>
         </ul>    
-    </aside>
+    </fieldset>
 
-    <main>
+    <fieldset>
+        <label>Instructions</label>
+
         <ul>
-            <h3>Instructions</h3>
-
             <?php foreach($recipe['recipeInstructions'] as $i => $step) {  ?>
                 <?php if(is_array($step) && isset($step['text'])) { $step = $step['text']; } ?>
 
@@ -69,7 +78,7 @@
             <?php } ?>
             <button class="icon-add"></button>
         </ul>
-    </main>
+    </fieldset>
 
     <button type="submit">Save</button>
 </form>
