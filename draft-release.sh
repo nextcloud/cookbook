938bda253e6555a365d8f2c7322e286decbc1651
Jeppe Zapp <contact@jeppezapp.com>
2019-08-09 08:38:04 +0200

    Added l10n to build script

--- draft-release.sh
+++ draft-release.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
+echo ""
+echo "Compiling translations..."
+echo ""
+
+wget https://github.com/nextcloud/docker-ci/blob/master/translations/translationtool/translationtool.phar?raw=true -O translationtool.phar 
+
+mkdir l10n
+
+php translationtool.phar convert-po-files
+
+rm translationtool.phar
+
+echo ""
+echo "Creating archive..."
+echo ""
+
 ln -s . cookbook
 tar -czvf cookbook.tar.gz cookbook/appinfo cookbook/composer.json cookbook/COPYING cookbook/css cookbook/img cookbook/js cookbook/lib cookbook/Makefile cookbook/phpunit.integration.xml cookbook/phpunit.xml cookbook/README.md cookbook/templates cookbook/tests
 rm cookbook
 
+echo ""
+echo "Creating signing key..."
+echo ""
+
 openssl dgst -sha512 -sign ~/.nextcloud/certificates/cookbook.key cookbook.tar.gz | openssl base64
+
+echo ""
+echo "Done!"
+echo ""
