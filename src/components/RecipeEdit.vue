b52d2adacbe7242811f31045c55ad7492f015f95
Sampsa Lohi <26563023+sam-19@users.noreply.github.com>
2020-06-12 21:58:31 +0300

    One stray translation found

--- src/components/RecipeEdit.vue
+++ src/components/RecipeEdit.vue
@@ -3,7 +3,7 @@
         <EditInputField :fieldName="'name'" :fieldType="'text'" :fieldLabel="t('cookbook', 'Name')" />
         <EditInputField :fieldName="'description'" :fieldType="'textarea'" :fieldLabel="t('cookbook', 'Description')" />
         <EditInputField :fieldName="'url'" :fieldType="'url'" :fieldLabel="t('cookbook', 'URL')" />
-        <EditImageField :fieldName="'image'" :fieldLabel="('Image')" />
+        <EditImageField :fieldName="'image'" :fieldLabel="t('cookbook', 'Image')" />
         <EditTimeField :fieldName="'prepTime'" :fieldLabel="t('cookbook', 'Preparation time')" />
         <EditTimeField :fieldName="'cookTime'" :fieldLabel="t('cookbook', 'Cooking time')" />
         <EditTimeField :fieldName="'totalTime'" :fieldLabel="t('cookbook', 'Total time')" />
