2d1417bf81d2780fe9ae58e56313d1a598733e81
Christian Wolf <github@christianwolf.email>
2020-10-06 18:42:44 +0200

    Copied over the main tests from travis to action code

--- .github/actions/run-tests/tests/test-entrypoint.sh
+++ .github/actions/run-tests/tests/test-entrypoint.sh
@@ -9,11 +9,66 @@ echo "Preparing the build system"
 # Prepare the system
 npm install -g npm@latest
 
-# Build the system
+cd nextcloud
 
-mount
+echo "Build the app"
+pushd custom_apps/cookbook
+make
+popd
 
-pwd
+echo "Prepare database"
+
+function call_mysql()
+{
+	mysql -u tester -h mysql -ptester_pass "$@"
+}
+
+case "$INPUT_DB" in
+	mysql)
+		for i in `seq 1 10`
+		do
+			call_mysql -e 'SHOW PROCESSLIST;' && break || true
+			sleep 5
+		done
+		if [ $i -eq 10 ]; then
+			echo '::error ::Could not connect to mysql database'
+			exit 1
+		fi
+		
+		;;
+	*)
+		echo "::warning ::No database specific initilization in test script. This might be a bug."
+		;;
+esac
+
+echo "Initialize nextcloud instance"
+mkdir data
+
+case "$INPUT_DB" in
+	mysql)
+		./occ maintenance:install --database-name nc_test --database-user tester --admin-user admin --admin-pass admin --database mysql --database-pass 'tester_pass'
+		;;
+	*)
+		echo "::error ::No database specific initilization in test script. This might be a bug."
+		exit 1
+		;;
+esac
+
+echo 'Installing the cookbook app'
+
+./occ app:enable cookbook
+
+echo 'Starting a temporary web server'
+php -S localhost:8080 &
+pid=$!
+
+cd custom_apps/cookbook
+
+make test
+
+kill $pid
+
+###############
 echo
 ls -la
 echo
