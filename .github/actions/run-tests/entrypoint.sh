6b2b3b98896bd14b34b9b0b2aeae737994a60ab7
Christian Wolf <github@christianwolf.email>
2020-10-06 16:12:19 +0200

    Test to pass variables from inputs to docker actions

--- .github/actions/run-tests/entrypoint.sh
+++ .github/actions/run-tests/entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-# env
+env
 
 cd /tests
 echo "Creating test docker image with the following settings"
 echo "PHP version: $INPUT_PHPVERSION"
 
-docker build -t docker-tests --build-arg PHPVERSION="$INPUT_PHPVERSION" .
-docker run -e INPUT_DB -v $GITHUB_WORKSPACE:/workdir docker-tests
+echo docker build -t docker-tests --build-arg PHPVERSION="$INPUT_PHPVERSION" .
+echo docker run -e INPUT_DB -v "$HOST_GITHUB_WORKSPACE":"/workdir" docker-tests
