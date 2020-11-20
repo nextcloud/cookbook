73b6584ee8642c72ff579dcecbab7184da8aba95
Christian Wolf <github@christianwolf.email>
2020-11-20 18:06:17 +0100

    Implemented search for recipe by id
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- lib/Db/RecipeDbWrapper.php
+++ lib/Db/RecipeDbWrapper.php
@@ -4,6 +4,8 @@ namespace  OCA\Cookbook\Db;
 
 use OCA\Cookbook\Entity\RecipeEntity;
 use OCP\IDBConnection;
+use OCA\Cookbook\Exception\EntityNotFoundException;
+use OCP\IL10N;
 
 class RecipeDbWrapper extends AbstractDbWrapper {
 	
@@ -17,25 +19,66 @@ class RecipeDbWrapper extends AbstractDbWrapper {
 	 */
 	private $userId;
 	
-	public function __construct(string $UserId, IDBConnection $db) {
+	/**
+	 * @var IL10N
+	 */
+	private $l;
+	
+	public function __construct(string $UserId, IDBConnection $db, IL10N $l) {
 		parent::__construct($db);
 		$this->db = $db;
 		$this->userId = $UserId;
+		$this->l = $l;
 	}
 	
 	protected function fetchDatabase(): array {
 		// FIXME
+		$qb = $this->db->getQueryBuilder();
+		
+		$qb ->select('id', 'name')
+			->from('cookbook_names')
+			->where('user_id = :uid');
+		$qb->setParameter('uid', $this->userId);
+		
+		$res = $qb->execute();
+		$ret = [];
+		
+		while($row = $res->fetch())
+		{
+			$recipe = $this->createEntity(); // XXX Better create directly?
+			$recipe->setName($row['name']);
+			$recipe->setId($row['id']);
+			
+			$ret[] = $recipe;
+		}
+		
+		return $ret;
 	}
-
+	
 	public function store(RecipeEntity $recipe): void {
 		// FIXME
 	}
 	
 	public function createEntity(): RecipeEntity {
-		// FIXME
+		return new RecipeEntity($this);
 	}
 	
 	public function remove(RecipeEntity $recipe): void {
 		// FIXME
+	}
+	
+	public function getRecipeById(int $id): RecipeEntity {
+		$entities = $this->getEntries();
+		
+		foreach($entities as $entry)
+		{
+			/**
+			 * @var RecipeEntity $entry
+			 */
+			if($entry->getId() == $id)
+				return $entry;
+		}
+		
+		throw new EntityNotFoundException($this->l->t('Recipe with id %d was not found.', $id));
 	}
 }
---
+++ lib/Exception/EntityNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OCA\Cookbook\Exception;
+
+class EntityNotFoundException extends \Exception {
+	/**
+     * {@inheritDoc}
+     * @see \Exception::__construct()
+     */
+    public function __construct($message = null, $code = null, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+	
+	
+}
