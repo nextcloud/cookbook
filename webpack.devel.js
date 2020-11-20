ec13b0c6676184c7d089c825d1f885be7870c762
thembeat <thembeat@gmx.de>
2020-11-20 09:33:57 +0100

    Reduce Bundlesize and add Bundle-Analyzer
    Signed-off-by: thembeat <thembeat@gmx.de>
    
    	modified:   package.json
    	modified:   webpack.build.js
    	modified:   webpack.config.js
    	modified:   webpack.devel.js

--- webpack.devel.js
+++ webpack.devel.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.config.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-cheap-source-map',
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
 })
