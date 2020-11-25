1fb9d693b601a37a75256155b0092675e36a4a1c
Sebastian Fey <info@sebastianfey.de>
2020-11-25 23:49:23 +0100

    Added component for displaying nutrition info in recipe
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/components/RecipeNutritionInfoItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <li>
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="data">
+            {{ data }}
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'RecipeNutritionInfoItem',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        data: {
+            type: String,
+            required: true
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+li {
+    margin-left: 1.25em;
+    margin-bottom: .5em;
+}
+
+li .title {
+    margin-bottom: 0em;
+    color: var(--color-primary);
+}
+
+</style>
