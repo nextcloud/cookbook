c9a77523d5245390e82bb8f3cfbfd65d13b0810c
Christian Wolf <github@christianwolf.email>
2020-12-20 12:51:17 +0100

    Corrected structure to represent most current codebase
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- .github/actions/deploy/fill-in-data.sh
+++ .github/actions/deploy/fill-in-data.sh
@@ -3,4 +3,7 @@
 version="$1"
 
 echo "Updating info.xml"
-cat .github/actions/deploy/info.xml.dist | sed "s/%%VERSION%%/$version/g" > appinfo/info.xml
+cat .github/actions/deploy/appinfo/info.xml.dist | sed "s/%%VERSION%%/$version/g" > appinfo/info.xml
+
+echo "Updating package.json"
+sed "s/\"version\": \\?\"[^\"]*\"/\"version\": \"$version\"/g" -i package.json
