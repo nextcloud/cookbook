f6349a615b2af1d82a1eb8b3e413e5534cb11e69
Sebastian Fey <info@sebastianfey.de>
2021-01-16 15:39:42 +0100

    Created recipe-list component
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "RecipeList",
+    props: ['recipes'],
+    data() {
+        return {}
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
