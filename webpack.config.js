a6efd0783a545bcf5f4c21afdad1d2eb8a687781
Sebastian Fey <info@sebastianfey.de>
2021-01-24 16:53:10 +0100

    Added packages for SCSS support
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,16 @@ module.exports = {
                 test: /\.svg$/,
                 loader: 'svg-inline-loader'
             },
+            // this will apply to both plain `.scss` files
+            // AND `<style lang="scss">` blocks in `.vue` files
+            {
+                test: /\.scss$/,
+                use: [
+                'vue-style-loader',
+                'css-loader',
+                'sass-loader'
+                ]
+            }
         ],
     },
     plugins: [
