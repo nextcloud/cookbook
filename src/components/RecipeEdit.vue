7541d04aba05c73c1ff338e2d427c6600117f892
Sebastian Fey <info@sebastianfey.de>
2021-01-24 16:54:19 +0100

    Avoid intermediate showing of overflow bars of the browser
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/components/RecipeEdit.vue
+++ src/components/RecipeEdit.vue
@@ -457,6 +457,8 @@ beforeRouteLeave (to, from, next) {
     position: absolute;
     width: 100%;
     height: 100%;
+    left: 0;
+    top: 0;
     background-color: var(--color-main-background);
     opacity: .75;
     z-index: 1000;
