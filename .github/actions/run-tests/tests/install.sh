b2b3e858e257c22959753a0d760a6fbea03c4d99
Christian Wolf <github@christianwolf.email>
2021-02-07 10:57:10 +0100

    Install composer from upstream
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- .github/actions/run-tests/tests/install.sh
+++ .github/actions/run-tests/tests/install.sh
@@ -49,3 +49,17 @@ docker-php-ext-enable xdebug
 echo 'runner ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 npm install -g npm@latest
+
+# Install composer
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', '/tmp/composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    exit 1
+fi
+
+php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm /tmp/composer-setup.php
