b85f4af5d8c7977cabcffc51f6836eded2fc8373
Christian Wolf <github@christianwolf.email>
2021-07-01 11:47:04 +0200

    Update schema to cope well with shared recipes
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ lib/Migration/Version000000Date20210701093123.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Cookbook\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000000Date20210701093123 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+	    /**
+	     * @var ISchemaWrapper $schema
+	     */
+	    $schema = $schemaClosure();
+	    
+	    $namesTable = $schema->getTable('cookbook_names');
+	    if ($namesTable->hasPrimaryKey()) {
+	        $namesTable->dropPrimaryKey();
+	    }
+	    if (! $namesTable->hasIndex('names_recipe_idx')) {
+	        $namesTable->addUniqueIndex([
+	            'recipe_id',
+	            'user_id'
+	        ], 'names_recipe_idx');
+	    }
+	    
+	    return $schema;
+	}
+
+}
