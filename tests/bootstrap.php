193304ec68d7760cfa9bde86508e5a443b23da6c
Christian Wolf <github@christianwolf.email>
2021-07-26 13:14:56 +0200

    Corrected code styling
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,13 +8,13 @@ require_once __DIR__ . '/../../../tests/bootstrap.php';
 function resetEnvironmentToBackup(string $name = 'default', bool $forceprint = false) {
 	$output = [];
 	$ret = -1;
-    exec("./.github/actions/run-tests/reset-from-container.sh $name 2>&1", $output, $ret);
+	exec("./.github/actions/run-tests/reset-from-container.sh $name 2>&1", $output, $ret);
 	if ($ret !== 0 || $forceprint) {
 		echo "\nStandard output:\n";
 		print_r($output);
 		echo "Return value: $ret\n";
-		if($ret !== 0) {
-		  throw new Exception("Could not reset environment");
+		if ($ret !== 0) {
+			throw new Exception("Could not reset environment");
 		}
 	}
 }
@@ -22,14 +22,16 @@ function resetEnvironmentToBackup(string $name = 'default', bool $forceprint = f
 function runOCCCommand(array $args, bool $forceprint = false) {
 	$output = [];
 	$ret = -1;
-	$params = join(' ', array_map(function($x){return escapeshellarg($x);}, $args));
-    exec("./.github/actions/run-tests/run-occ.sh $params 2>&1", $output, $ret);
+	$params = join(' ', array_map(function ($x) {
+		return escapeshellarg($x);
+	}, $args));
+	exec("./.github/actions/run-tests/run-occ.sh $params 2>&1", $output, $ret);
 	if ($ret !== 0 || $forceprint) {
 		echo "\nStandard output:\n";
 		print_r($output);
 		echo "Return value: $ret\n";
-		if($ret !== 0) {
-		  throw new Exception("Could not run OCC command");
+		if ($ret !== 0) {
+			throw new Exception("Could not run OCC command");
 		}
 	}
 }
