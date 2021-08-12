33a87ce06589e560cee3d2ccdda8bd6944634a23
Christian Wolf <github@christianwolf.email>
2021-08-12 17:16:55 +0200

    Enhance debugging environment
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,7 +25,10 @@ function runOCCCommand(array $args, bool $forceprint = false) {
 	$params = join(' ', array_map(function ($x) {
 		return escapeshellarg($x);
 	}, $args));
-	exec("./.github/actions/run-tests/run-occ.sh $params 2>&1", $output, $ret);
+	
+	$cmd = "./.github/actions/run-tests/run-occ.sh $params 2>&1";
+	
+	exec($cmd, $output, $ret);
 	if ($ret !== 0 || $forceprint) {
 		echo "\nStandard output:\n";
 		print_r($output);
