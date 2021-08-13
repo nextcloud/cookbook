582eda951c2ac680e4c478ff3114330717293c71
Christian Wolf <github@christianwolf.email>
2021-08-13 12:17:29 +0200

    Created basic test code for existing migrations
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ tests/Integration/Setup/Migrations/Version000000Date20190312140601Test.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OCA\Cookbook\tests\Integration\Setup\Migrations;
+
+include_once __DIR__ . '/AbstractMigrationTestCase.php';
+
+class Version000000Date20190312140601Test extends AbstractMigrationTestCase {
+	
+	/**
+	 * @runInSeparateProcess
+	 * @covers \OCA\Cookbook\Migration\Version000000Date20190312140601
+	 */
+	public function testRedundantEntriesInDB($data, $updatedUsers) {
+		
+		// Run the migration under test
+		$this->migrationService->migrate('000000Date20190312140601');
+		
+		$this->postTestAsserts('cookbook_recipes');
+		$this->postTestAsserts('cookbook_keywords');
+	}
+	
+	private function preTestAsserts(string $tableName): void {
+		$this->assertTrue($this->schema->hasTable($tableName));
+		$table = $this->schema->getTable($tableName);
+		$this->assertFalse($table->hasColumn('user_id'));
+	}
+	
+	private function postTestAsserts(string $tableName): void {
+		$this->assertTrue($this->schema->hasTable($tableName));
+		
+		$table = $this->schema->getTable($tableName);
+		$this->assertTrue($table->hasColumn('recipe_id'));
+		$this->assertTrue($table->hasColumn('name'));
+		
+		$this->assertTrue($table->getColumn('user_id')->getNotnull());
+		$this->assertTrue($table->getColumn('name')->getNotnull());
+	}
+	
+	protected function getPreviousMigrationName(): string {
+		return null;
+	}
+}
---
+++ tests/Integration/Setup/Migrations/Version000000Date20200315121603Test.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OCA\Cookbook\tests\Integration\Setup\Migrations;
+
+include_once __DIR__ . '/AbstractMigrationTestCase.php';
+
+class Version000000Date20200315121603Test extends AbstractMigrationTestCase {
+	
+	/**
+	 * @runInSeparateProcess
+	 * @covers \OCA\Cookbook\Migration\Version000000Date20200315121603
+	 */
+	public function testRedundantEntriesInDB($data, $updatedUsers) {
+		$this->assertFalse($this->schema->hasTable('cookbook_categories'));
+		
+		// Run the migration under test
+		$this->migrationService->migrate('000000Date20210427082010');
+		
+		$this->assertTrue($this->schema->hasTable('cookbook_categories'));
+		
+		$table = $this->schema->getTable('cookbook_categories');
+		
+		$this->assertTrue($table->hasColumn('recipe_id'));
+		$this->assertTrue($table->hasColumn('name'));
+		$this->assertTrue($table->hasColumn('user_id'));
+		
+		$this->assertTrue($table->getColumn('recipe_id')->getNotnull());
+		$this->assertTrue($table->getColumn('name')     ->getNotnull());
+		$this->assertTrue($table->getColumn('user_id')  ->getNotnull());
+	}
+	
+	protected function getPreviousMigrationName(): string {
+		return '000000Date20190910223344';
+	}
+}
