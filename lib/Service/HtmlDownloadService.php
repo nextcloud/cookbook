7b9f60bb47a018d86b021c45771fed7f68e51544
Christian Wolf <github@christianwolf.email>
2021-09-02 13:29:31 +0200

    Adding a download service
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- lib/Service/HtmlDownloadService.php
+++ lib/Service/HtmlDownloadService.php
@@ -2,34 +2,15 @@
 
 namespace OCA\Cookbook\Service;
 
+use DOMDocument;
 use OCA\Cookbook\Exception\ImportException;
 use OCA\Cookbook\Helper\HTMLFilter\AbstractHtmlFilter;
 use OCA\Cookbook\Helper\HTMLFilter\HtmlEntityDecodeFilter;
+use OCA\Cookbook\Helper\HtmlToDomParser;
 use OCP\IL10N;
 use OCP\ILogger;
 
 class HtmlDownloadService {
-	
-	/**
-	 * Indicates the parsing was successfully terminated
-	 * @var integer
-	 */
-	public const PARSING_SUCCESS = 0;
-	/**
-	 * Indicates the parsing terminated with warnings
-	 * @var integer
-	 */
-	public const PARSING_WARNING = 1;
-	/**
-	 * Indicates the parsing terminated with an error
-	 * @var integer
-	 */
-	public const PARSING_ERROR = 2;
-	/**
-	 * Indicates that the parsing terminated with a fatal error
-	 * @var integer
-	 */
-	public const PARSING_FATAL_ERROR = 3;
 	
 	/**
 	 * @var array
@@ -47,23 +28,21 @@ class HtmlDownloadService {
 	private $l;
 	
 	/**
-	 * @var \DOMDocument
+	 * @var HtmlToDomParser
+	 */
+	private $htmlParser;
+	
+	/**
+	 * @var DOMDocument
 	 */
 	private $dom;
 	
-	/**
-	 * The parsed HTML content
-	 * @var string
-	 */
-	private $html;
-	
 	public function __construct(HtmlEntityDecodeFilter $htmlEntityDecodeFilter,
-		ILogger $logger, IL10N $l10n) {
+		ILogger $logger, IL10N $l10n, HtmlToDomParser $htmlParser) {
 		$this->htmlFilters = [ $htmlEntityDecodeFilter ];
 		$this->logger = $logger;
 		$this->l = $l10n;
-		$this->dom = null;
-		$this->html = null;
+		$this->htmlParser = $htmlParser;
 	}
 	
 	/**
@@ -77,19 +56,17 @@ class HtmlDownloadService {
 	 * @throws ImportException If obtaining of the URL was not possible
 	 */
 	public function downloadRecipe(string $url): int {
-		// Reset in case of exception
-		$this->dom = null;
-		$this->html = null;
-		
-		$this->html = $this->fetchHtmlPage($url);
+		$html = $this->fetchHtmlPage($url);
 		
 		// Filter the HTML code
 		/** @var AbstractHtmlFilter $filter */
 		foreach ($this->htmlFilters as $filter) {
-			$filter->apply($this->html);
+			$filter->apply($html);
 		}
 		
-		return $this->loadHtmlString($url);
+		$dom = new DOMDocument();
+		$this->dom = $this->htmlParser->loadHtmlString($dom, $url, $html);
+		return $this->htmlParser->getState();
 	}
 	
 	/**
@@ -98,13 +75,6 @@ class HtmlDownloadService {
 	 */
 	public function getDom(): ?\DOMDocument {
 		return $this->dom;
-	}
-
-	/**
-	 * @return string The parsed HTML or null if no parsing was successful
-	 */
-	public function getHtml(): ?string {
-		return $this->html;
 	}
 
 	/**
@@ -138,90 +108,4 @@ class HtmlDownloadService {
 		return $html;
 	}
 	
-	/**
-	 * @param string $url The URL of the parsed recipe
-	 * @throws ImportException If the parsing of the HTML page failed completely
-	 * @return int Indicator of the parsing state
-	 */
-	private function loadHtmlString(string $url): int {
-		$this->dom = new \DOMDocument();
-		
-		$libxml_previous_state = libxml_use_internal_errors(true);
-		
-		try {
-			$parsedSuccessfully = $this->dom->loadHTML($this->html);
-			
-			// Error handling
-			$errors = libxml_get_errors();
-			$result = $this->checkXMLErrors($errors, $url);
-			libxml_clear_errors();
-			
-			if (!$parsedSuccessfully) {
-				throw new ImportException($this->l->t('Parsing of HTML failed.'));
-			}
-		} finally {
-			libxml_use_internal_errors($libxml_previous_state);
-		}
-		
-		return $result;
-	}
-	
-	/**
-	 * Compress the xml errors to small mount of log messages
-	 *
-	 * The return value indicates what the most critical value was.
-	 * The value can be PARSING_SUCCESS, PARSING_WARNING, PARSING_ERROR, or PARSING_FATAL_ERROR.
-	 *
-	 * @param array $errors The array of all parsed errors
-	 * @param string $url The parsed URL
-	 * @return int Indicator what the most severe issue was
-	 */
-	private function checkXMLErrors(array $errors, string $url): int {
-		$error_counter = [];
-		$by_error_code = [];
-		
-		foreach ($errors as $error) {
-			if (array_key_exists($error->code, $error_counter)) {
-				$error_counter[$error->code] ++;
-			} else {
-				$error_counter[$error->code] = 1;
-				$by_error_code[$error->code] = $error;
-			}
-		}
-		
-		$return = self::PARSING_SUCCESS;
-		
-		/**
-		 * @var int $code
-		 * @var int $count
-		 */
-		foreach ($error_counter as $code => $count) {
-			/** @var \LibXMLError $error */
-			$error = $by_error_code[$code];
-			
-			switch ($error->level) {
-				case LIBXML_ERR_WARNING:
-					$error_message = $this->l->t('Warning %u occurred %u times while parsing %s.', [$error->code, $count, $url]);
-					$return = max($return, self::PARSING_WARNING);
-					break;
-				case LIBXML_ERR_ERROR:
-					$error_message = $this->l->t('Error %u occurred %u times while parsing %s.', [$error->code, $count, $url]);
-					$return = max($return, self::PARSING_ERROR);
-					break;
-				case LIBXML_ERR_FATAL:
-					$error_message = $this->l->t('Fatal error %u occurred %count$u times while parsing %s.', [$error->code, $count, $url]);
-					$return = max($return, self::PARSING_FATAL_ERROR);
-					break;
-				default:
-					throw new \Exception($this->l->t('Unsupported error level during parsing of XML output.'));
-			}
-			
-			$last_occurence = $this->l->t('Last time it occurred in line %u and column %u', [$error->line, $error->column]);
-			
-			$error_message = "libxml: $error_message $last_occurence: " . $error->message;
-			$this->logger->warning($error_message);
-		}
-		
-		return $return;
-	}
 }
