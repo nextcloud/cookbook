83af7e89f9c05896c5f2756eb4d71b364b721ef6
Christian Wolf <github@christianwolf.email>
2022-03-24 19:50:17 +0100

    Created helper classes for management of image files
    Added tests for helper classes
    
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ lib/Helper/ImageService/ImageFileHelper.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace OCA\Cookbook\Helper\ImageService;
+
+use OCP\IL10N;
+use OCP\Files\File;
+use OCP\Files\Folder;
+use OCP\Files\NotFoundException;
+use OCA\Cookbook\Exception\RecipeImageExistsException;
+
+class ImageFileHelper {
+	private const NAME_MAIN = ImageSize::NAME_MAP[ImageSize::PRIMARY_IMAGE];
+
+	/**
+	 * @var IL10N
+	 */
+	private $l;
+
+	public function __construct(
+		IL10N $l
+		) {
+		$this->l = $l;
+	}
+
+	/**
+	 * Get the main image from a recipe
+	 *
+	 * @param Folder $recipeFolder The folder of the recipe
+	 * @return File The image file
+	 * @throws NotFoundException if no image has been found
+	 * @todo Do not use low-level functions of NC core here.
+	 */
+	public function getImage(Folder $recipeFolder): File {
+		return $recipeFolder->get(self::NAME_MAIN);
+	}
+
+	/**
+	 * Check if a recipe has a folder attached
+	 *
+	 * @param Folder $recipeFolder The folder of the recipe to check
+	 * @return boolean true, if there is an image present
+	 */
+	public function hasImage(Folder $recipeFolder): bool {
+		return $recipeFolder->nodeExists(self::NAME_MAIN);
+	}
+
+	/**
+	 * Drop the image of a recipe
+	 *
+	 * @param Folder $recipeFolder The folder contraining the recipe
+	 * @return void
+	 */
+	public function dropImage(Folder $recipeFolder): void {
+		if ($recipeFolder->nodeExists(self::NAME_MAIN)) {
+			$recipeFolder->get(self::NAME_MAIN)->delete();
+		}
+	}
+
+	/**
+	 * Create an image for a recipe
+	 *
+	 * @param Folder $recipeFolder The folder containing the recipe files.
+	 * @return File The image object for the recipe
+	 * @throws RecipeImageExistsException if the folder has already a image file present.
+	 */
+	public function createImage(Folder $recipeFolder): File {
+		if ($this->hasImage($recipeFolder)) {
+			throw new RecipeImageExistsException(
+				$this->l->t('The recipe has already an image file. Cannot create a new one.')
+			);
+		} else {
+			return $recipeFolder->newFile(self::NAME_MAIN);
+		}
+	}
+}
---
+++ tests/Unit/Helper/ImageService/ImageFileHelperTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace OCA\Cookbook\tests\unit\Helper\ImageService;
+
+use OCA\Cookbook\Exception\RecipeImageExistsException;
+use OCP\IL10N;
+use OCP\Files\File;
+use OCP\Files\Folder;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\Stub;
+use PHPUnit\Framework\MockObject\MockObject;
+use OCA\Cookbook\Helper\ImageService\ImageFileHelper;
+use OCP\Files\NotFoundException;
+
+/**
+ * @covers OCA\Cookbook\Helper\ImageService\ImageFileHelper
+ * @covers OCA\Cookbook\Exception\RecipeImageExistsException
+ */
+class ImageFileHelperTest extends TestCase {
+	
+	/**
+	 * @var ImageFileHelper
+	 */
+	private $dut;
+
+	/**
+	 * @var MockObject|Folder
+	 */
+	private $folder;
+
+	protected function setUp(): void {
+		/**
+		 * @var Stub|IL10N $l
+		 */
+		$l = $this->createStub(IL10N::class);
+		$l->method('t')->willReturnArgument(0);
+
+		$this->folder = $this->createMock(Folder::class);
+
+		$this->dut = new ImageFileHelper($l);
+	}
+
+	public function dpExisting() {
+		yield [true];
+		yield [false];
+	}
+
+	/**
+	 * @dataProvider dpExisting
+	 */
+	public function testHasImage($present) {
+		$this->folder->method('nodeExists')->with('full.jpg')->willReturn($present);
+
+		$this->assertEquals($present, $this->dut->hasImage($this->folder));
+	}
+
+	public function testGetImage() {
+		$file = $this->createStub(File::class);
+		$this->folder->method('get')->with('full.jpg')->willReturn($file);
+		$this->assertSame($file, $this->dut->getImage($this->folder));
+	}
+
+	/**
+	 * @dataProvider dpExisting
+	 */
+	public function testDropImage($existing) {
+		/**
+		 * @var MockObject|File $file
+		 */
+		$file = $this->createMock(File::class);
+		$this->folder->method('nodeExists')->with('full.jpg')->willReturn($existing);
+		
+		if ($existing) {
+			$this->folder->method('get')->with('full.jpg')->willReturn($file);
+			$file->expects($this->once())->method('delete');
+		} else {
+			$this->folder->method('get')->with('full.jpg')->willThrowException(new NotFoundException());
+			$file->expects($this->never())->method('delete');
+		}
+
+		$this->dut->dropImage($this->folder);
+	}
+
+	/**
+	 * @dataProvider dpExisting
+	 */
+	public function testCreateImage($existing) {
+		$this->folder->method('nodeExists')->with('full.jpg')->willReturn($existing);
+		$file = $this->createStub(File::class);
+		$this->folder->method('newFile')->with('full.jpg')->willReturn($file);
+
+		if ($existing) {
+			$this->expectException(RecipeImageExistsException::class);
+		}
+
+		$this->assertSame($file, $this->dut->createImage($this->folder));
+	}
+}
