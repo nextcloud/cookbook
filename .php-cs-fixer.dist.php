7df8f0084d9b9c07fd559821590671aa5d624338
Christian Wolf <github@christianwolf.email>
2022-05-21 06:51:01 +0200

    Enabled PSR12 code styling
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,7 +6,21 @@ require_once './vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
-$config = new Config();
+class CookbookConfig extends Config {
+	public function __construct($name = 'default') {
+		parent::__construct($name);
+	}
+
+	public function getRules() : array {
+		$parentRules = parent::getRules();
+		$additionalRules = [
+			// 'no_whitespace_in_blank_line' => true,
+		];
+		return array_merge(['@PSR12' => true], $parentRules, $additionalRules);
+	}
+}
+
+$config = new CookbookConfig();
 $config
 	->getFinder()
 	->ignoreVCSIgnored(true)
@@ -18,5 +32,7 @@ $config
 	->exclude('vendor')
 	->exclude('.github')
 	->in(__DIR__);
+
+
 return $config;
 
