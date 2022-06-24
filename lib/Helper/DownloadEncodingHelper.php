8a3833022655aed68fe5c6852c0846831f46b0fe
Christian Wolf <github@christianwolf.email>
2022-06-24 11:58:25 +0200

    Add some docuemntation
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- lib/Helper/DownloadEncodingHelper.php
+++ lib/Helper/DownloadEncodingHelper.php
@@ -2,7 +2,17 @@
 
 namespace OCA\Cookbook\Helper;
 
+/**
+ * This class handles the encoding of downloads in order to only work with UTF8 strings.
+ */
 class DownloadEncodingHelper {
+	/**
+	 * Encode a string to UTF8
+	 *
+	 * @param string $data The data to be converted
+	 * @param string $encoding The encoding of the input string
+	 * @return string The string encoded in UTF8 encoding
+	 */
 	public function encodeToUTF8(string $data, string $encoding): string {
 		$data = iconv($encoding, 'UTF-8', $data);
 		return $data;
