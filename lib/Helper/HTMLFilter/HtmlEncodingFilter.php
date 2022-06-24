c862b15d53211896f52ce881ba79ec71bc849f62
Christian Wolf <github@christianwolf.email>
2022-06-24 18:01:28 +0200

    Fix regex and added tests
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- lib/Helper/HTMLFilter/HtmlEncodingFilter.php
+++ lib/Helper/HTMLFilter/HtmlEncodingFilter.php
@@ -4,7 +4,7 @@ namespace OCA\Cookbook\Helper\HTMLFilter;
 
 class HtmlEncodingFilter extends AbstractHtmlFilter {
 	public function apply(string &$html): void {
-		if (preg_match('/^<?xml encoding=/', $html) === 0) {
+		if (preg_match('/^<\?xml/', $html) === 0) {
 			$html = '<?xml encoding="UTF-8">' . $html;
 		}
 	}
