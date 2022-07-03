b615ebfd6bfeeae2bf4fdbb9f3d69653e59a6131
Christian Wolf <github@christianwolf.email>
2022-07-03 21:24:57 +0200

    Added basic migration to add date columns to DB
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ lib/Migration/Version000000Date20220703174647.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Cookbook\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000000Date20220703174647 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('cookbook_names');
+		
+		if(! $table->hasColumn('dateCreated')) {
+			$table->addColumn('dateCreated', 'date', [
+				'notnull' => false,
+			]);
+		}
+
+		if(!$table->hasColumn('dateModified')) {
+			$table->addColumn('dateModified', 'date', [
+				'notnull' => false,
+			]);
+		}
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+	}
+}
---
+++ tests/Migration/Setup/Migrations/Version000000Date20220703174647Test.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace OCA\Cookbook\tests\Migration\Setup\Migrations;
+
+use OCP\DB\QueryBuilder\IQueryBuilder;
+
+include_once __DIR__ . '/AbstractMigrationTestCase.php';
+
+class Version000000Date20220703174647Test extends AbstractMigrationTestCase {
+	/**
+	 * dataProvider dataProvider
+	 * @runInSeparateProcess
+	 * @covers \OCA\Cookbook\Migration\Version000000Date20220703174647
+	 * @param mixed $data
+	 * @param mixed $updatedUsers
+	 */
+	public function testRedundantEntriesInDB(/*$data, $updatedUsers*/) {
+		// Add recipe dummy data from data provider
+		$qb = $this->db->getQueryBuilder();
+		$qb->insert('cookbook_names')
+			->values([
+				'recipe_id' => ':recipe',
+				'user_id' => ':user',
+				'name' => ':name',
+			]);
+		$qb->setParameter('name', 'name of the recipe');
+		$qb->setParameter('user', 'username');
+		$qb->setParameter('recipe', 1234);
+
+		$this->assertEquals(1, $qb->execute());
+
+		$table = $this->schema->getTable('cookbook_names');
+		$this->assertFalse($table->hasColumn('dateCreated'));
+		$this->assertFalse($table->hasColumn('dateModified'));
+
+		// Run the migration under test
+		$this->migrationService->migrate('000000Date20220703174647');
+		$this->renewSchema();
+
+		$table = $this->schema->getTable('cookbook_names');
+		$this->assertTrue($table->hasColumn('dateCreated'));
+		$this->assertTrue($table->hasColumn('dateModified'));
+
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('dateCreated', 'dateModified')->from('cookbook_names');
+		$res = $qb->execute();
+		$data = $res->fetchAll();
+
+		$this->assertEquals(1, count($data));
+		$row = $data[0];
+		$this->assertEquals(2, count($row));
+		$this->assertNull($row['dateCreated']);
+		$this->assertNull($row['dateModified']);
+		
+
+		//$this->assertEquals([null, null], $data[0]);
+	}
+
+
+	protected function getPreviousMigrationName(): string {
+		return '000000Date20210701093123';
+	}
+}
