9bbf0cdf1f1d5139525e66a047b5aad35781d796
Christian Wolf <github@christianwolf.email>
2022-07-26 15:06:55 +0200

    Cleaned out entry scripts
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@
 // Markdown
 import VueShowdown from "vue-showdown"
 
-import { generateUrl } from "@nextcloud/router"
-
 import Vue from "vue"
 
 import helpers from "cookbook/js/helper"
@@ -25,43 +23,35 @@ if (__webpack_use_dev_server__ || false) {
     __webpack_public_path__ = "http://127.0.0.1:3000/apps/cookbook/js/"
 }
 
-// eslint-disable-next-line func-names, import/newline-after-import
-;(function (OC, window) {
-    // Fetch Nextcloud nonce identifier for dynamic script loading
-    // eslint-disable-next-line camelcase,no-undef
-    __webpack_nonce__ = btoa(OC.requestToken)
+// Fetch Nextcloud nonce identifier for dynamic script loading
+// eslint-disable-next-line camelcase,no-undef
+__webpack_nonce__ = btoa(OC.requestToken)
 
-    // eslint-disable-next-line no-param-reassign
-    window.baseUrl = generateUrl("apps/cookbook")
+helpers.useRouter(router)
 
-    helpers.useRouter(router)
+// A simple function to sanitize HTML tags
+// eslint-disable-next-line no-param-reassign
+window.escapeHTML = helpers.escapeHTML
 
-    // A simple function to sanitize HTML tags
-    // eslint-disable-next-line no-param-reassign
-    window.escapeHTML = helpers.escapeHTML
+// Also make the injections available in Vue components
+Vue.prototype.$window = window
+Vue.prototype.OC = OC
 
-    // Also make the injections available in Vue components
-    Vue.prototype.$window = window
-    Vue.prototype.OC = OC
+// Markdown for Vue
+Vue.use(VueShowdown, {
+    // set default flavor for Markdown
+    flavor: "vanilla",
+})
 
-    // Markdown for Vue
-    Vue.use(VueShowdown, {
-        // set default flavor for Markdown
-        flavor: "vanilla",
-    })
+// Pass translation engine to Vue
+Vue.prototype.t = window.t
 
-    // Pass translation engine to Vue
-    Vue.prototype.t = window.t
-
-    // Start the app once document is done loading
-    document.addEventListener("DOMContentLoaded", () => {
-        const App = Vue.extend(AppMain)
-        new App({
-            store,
-            router,
-            beforeCreate() {
-                this.$store.commit("initializeStore")
-            },
-        }).$mount("#content")
-    })
-})(OC, window)
+// Start the app once document is done loading
+const App = Vue.extend(AppMain)
+new App({
+    store,
+    router,
+    beforeCreate() {
+        this.$store.commit("initializeStore")
+    },
+}).$mount("#content")
