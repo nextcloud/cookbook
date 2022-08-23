d1d3200df995e0646de0dcae67738cc6a3c14223
Christian Wolf <github@christianwolf.email>
2022-08-23 11:12:16 +0200

    Updated structure to support pre-releases in general
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- .github/actions/deploy/create-version.sh
+++ .github/actions/deploy/create-version.sh
@@ -10,9 +10,14 @@ master_branch=master
 major=$(cat "$deploy_path/major")
 minor=$(cat "$deploy_path/minor")
 patch=$(cat "$deploy_path/patch")
+suffix=$(cat "$deploy_path/suffix")
 
 # release
 message=$(git log HEAD~1...HEAD --max-count=1 --format='%s%n%b')
+
+parse_pre_release () {
+        echo "$1" | grep -E '%PRE-?RELEASE%' | head -n 1 | sed -E 's@.*%PRE-?RELEASE%([^%]+)%.*@\1@'
+}
 
 if echo "$message" | grep '%MAJOR%' > /dev/null ; then
         
@@ -20,15 +25,39 @@ if echo "$message" | grep '%MAJOR%' > /dev/null ; then
         let major=$major+1
         minor=0
         patch=0
+        suffix=''
         
 elif echo "$message" | grep '%MINOR%' > /dev/null; then
         
         echo 'Creating minor version'
         let minor=minor+1
         patch=0
+        suffix=''
         
 else
         
+        prerelease="$(parse_pre_release "$message")"
+
+        if [ -n "$suffix" ]; then
+                # The previous version was a pre-release
+
+                if [ -n "$prerelease" ]; then
+                        # A new pre-release should be published
+                        suffix="$prerelease"
+                else
+                        # A real release should be published
+                        suffix=''
+                fi
+        else
+                # The previous release was a real release
+                
+                let patch=$patch+1
+
+                if [ -n "$prerelease" ]; then
+                        # The next release is a pre-release
+                        suffix="$prerelease"
+                fi
+        fi
         echo 'Creating patch version'
         let patch=$patch+1
         
@@ -38,10 +67,14 @@ echo "Updating bumped version files"
 echo $major > "$deploy_path/major"
 echo $minor > "$deploy_path/minor"
 echo $patch > "$deploy_path/patch"
+echo "$suffix" > "$deploy_path/suffix"
 
-git add "$deploy_path/major" "$deploy_path/minor" "$deploy_path/patch"
+git add "$deploy_path/major" "$deploy_path/minor" "$deploy_path/patch" "$deploy_path/suffix"
 
 version="$major.$minor.$patch"
+if [ -n "$suffix" ]; then
+        version="$version-$suffix"
+fi
 echo "New version is $version."
 
 git config user.name 'Github actions bot'
--- .github/actions/deploy/update-data.sh
+++ .github/actions/deploy/update-data.sh
@@ -7,7 +7,27 @@ deploy_path='.github/actions/deploy'
 major=$(cat "$deploy_path/major")
 minor=$(cat "$deploy_path/minor")
 patch=$(cat "$deploy_path/patch")
+suffix=$(cat "$deploy_path/suffix")
 
 version="$major.$minor.$patch"
 
-"$deploy_path/fill-in-data.sh" "$version" "$major" "$minor" "$patch"
+if [ -n "$suffix" ]; then
+	version="$version-$suffix"
+fi
+
+fill_in_data () {
+	echo "Updating info.xml"
+	cat .github/actions/deploy/appinfo/info.xml.dist | sed "s/%%VERSION%%/$version/g" > appinfo/info.xml
+
+	echo "Updating package.json"
+	sed "s/\"version\": \\?\"[^\"]*\"/\"version\": \"$version\"/g" -i package.json
+
+	echo "Updating version in main controller"
+	local version_arr="$major, $minor, $patch"
+	if [ -n "$suffix" ]; then
+		version_arr="$version_arr, -$suffix"
+	fi
+	sed "/VERSION_TAG/s@[[].*[]]@[$version_arr]@" -i lib/Controller/MainController.php
+}
+
+fill_in_data
