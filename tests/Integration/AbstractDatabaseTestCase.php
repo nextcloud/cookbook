5e33007a737d5f80702ccafc41ff1cefbc3fb19c
Christian Wolf <github@christianwolf.email>
2022-09-07 14:10:11 +0200

    Create abstract test class for integration testing
    This is a first step for #1020.
    
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ tests/Integration/AbstractDatabaseTestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OCA\Cookbook\tests\Integration;
+
+use ChristophWurst\Nextcloud\Testing\TestCase;
+use ChristophWurst\Nextcloud\Testing\DatabaseTransaction;
+use OCA\Cookbook\AppInfo\Application;
+use Psr\Container\ContainerInterface;
+
+abstract class AbstractDatabaseTestCase extends TestCase {
+	use DatabaseTransaction;
+
+	/** @var ContainerInterface */
+	protected $ncContainer;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$app = new Application();
+		$this->ncContainer = $app->getContainer();
+	}
+
+	protected function query(string $name) {
+		return $this->ncContainer->get($name);
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
+
+		$this->resetDataFolder();
+	}
+
+	private function resetDataFolder() {
+		$cmd = "rsync -a --delete --delete-delay /dumps/current/plain/data/ /var/www/html/data";
+		$output = [];
+		$ret = 0;
+		exec($cmd, $output, $ret);
+
+		if ($ret !== 0) {
+			// The rsync did fail
+			echo "\nThe recovery of the data failed. Standard output:\n";
+			print_r($output);
+			echo "The return code was $ret.\n";
+		}
+	}
+}
