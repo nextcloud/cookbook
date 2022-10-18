8069a707648250261a32de5dc6f2db170123c96f
Marcel Robitaille <mail@marcelrobitaille.me>
2022-10-18 16:57:27 +0200

    Fix weird formatting of comment before semicolon
    The semicolon is needed here because the line below starts with (.
    JavaScript will attempt to call the line that now has a semicolon as a
    function.
    
    Signed-off-by: Marcel Robitaille <mail@marcelrobitaille.me>

--- src/main.js
+++ src/main.js
@@ -35,7 +35,9 @@ helpers.useRouter(router)
 const locale = document.documentElement
     .getAttribute("data-locale")
     .replace("_", "-")
-    .toLowerCase() // `en` is the default locale and cannot be dynamically imported. Will 404
+    .toLowerCase()
+
+// `en` is the default locale and cannot be dynamically imported. Will 404
 // https://github.com/moment/moment/issues/3624
 ;(locale === "en"
     ? Promise.resolve()
