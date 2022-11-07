52bfe681703b07b152c89bbf66584a5e7cab36ef
Christian Wolf <github@christianwolf.email>
2022-11-07 15:09:51 +0100

    Removed old Application code
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -7,31 +7,18 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\Util;
 
-// IBootstrap requies NC >= 20
-// Remove conditional once we end support for NC 19
-if (Util::getVersion()[0] >= 20) {
-	class Application extends App implements IBootstrap {
-		public const APP_ID = 'cookbook';
+class Application extends App implements IBootstrap {
+	public const APP_ID = 'cookbook';
 
-		public function __construct(array $urlParams = []) {
-			parent::__construct(self::APP_ID, $urlParams);
-		}
-
-		public function register(IRegistrationContext $context): void {
-			$context->registerSearchProvider(Provider::class);
-		}
-
-		public function boot(IBootContext $context): void {
-		}
+	public function __construct(array $urlParams = []) {
+		parent::__construct(self::APP_ID, $urlParams);
 	}
-} else {
-	class Application extends App {
-		public const APP_ID = 'cookbook';
 
-		public function __construct(array $urlParams = []) {
-			parent::__construct(self::APP_ID, $urlParams);
-		}
+	public function register(IRegistrationContext $context): void {
+		$context->registerSearchProvider(Provider::class);
+	}
+
+	public function boot(IBootContext $context): void {
 	}
 }
