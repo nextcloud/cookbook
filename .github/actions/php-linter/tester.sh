a89f3785c41ddb058368e36b94ae7525082f11be
Christian Wolf <github@christianwolf.email>
2023-12-05 13:36:40 +0100

    Create script to check PHP for syntac compatibility
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ .github/actions/php-linter/tester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd "$1"
+
+ret=0
+
+find lib -type f -name '*.php' -print0 | while IFS= read -d '' file
+do
+	php -l "$file" > /tmp/msg
+	retSingle=$?
+
+	if [ $retSingle -eq 0 ]
+	then
+		cat /tmp/msg | sed 's@^@::debug::@'
+	else
+		msg="$(cat /tmp/msg)"
+		echo "$msg" | sed 's@^\s*$@@' | grep -v '^$' > /tmp/msg
+		line=$(cat /tmp/msg | grep -o ' on line [0-9]*' | sed -E 's@ on line ([0-9]*)$@\1@')
+		prefix="::error file=$file,line=$line,title=PHP linter failed::"
+		sed "s@^@$prefix@" /tmp/msg 
+	fi
+done
+
+exit $ret
