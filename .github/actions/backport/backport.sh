3b0f2517ae98083d17aa38508314f11c9ab5df7a
Christian Wolf <github@christianwolf.email>
2023-12-05 19:45:57 +0100

    Squashed fix of the action
    Fix code of backport script
    
    Signed-off-by: Christian Wolf <github@christianwolf.email>
    
    Use correct SHAs
    
    Enable debug output
    
    Change checkout branch name
    
    Signed-off-by: Christian Wolf <github@christianwolf.email>
    
    Fix typo in clone action
    
    Use correct onto name
    
    Allow changes be done by workflow
    
    Use a PAT
    
    Only allow certain chars in branch names
    
    Signed-off-by: Christian Wolf <github@christianwolf.email>
    
    Fix quote issue
    
    Fix actions
    
    Add debug logging
    
    Put log in deicated step
    
    Make backport script compatible with pull request documentation
    
    Prevent /backport as code name
    
    change namespace

--- .github/actions/backport/backport.sh
+++ .github/actions/backport/backport.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [ "$DEBUG" = '1' ]
+then
+	set -x
+fi
+
 echo "::notice::Fetching PR information"
 curl -L -H "Accept: application/vnd.github+json" "https://api.github.com/repos/nextcloud/cookbook/pulls/$number" > /tmp/pr-data
 
@@ -7,20 +12,23 @@ baseSha=$(cat /tmp/pr-data | jq -r '.base.sha')
 echo "::debug::Base SHA is $baseSha"
 baseRef=$(cat /tmp/pr-data | jq -r '.base.ref')
 echo "::debug::Base reference is $baseRef"
-headSha=$(cat /tmp/pr-data | jq -r '.base.sha')
+headSha=$(cat /tmp/pr-data | jq -r '.head.sha')
 echo "::debug::Head SHA of rebase is $headSha"
+headRef=$(cat /tmp/pr-data | jq -r '.head.ref')
+echo "::debug::Reference of rebase is $headRef"
 
 IFS=' ' read tmp branchName < /tmp/comment
+branchName="$(echo "$branchName" | tr -cd "a-zA-Z0-9+=_/,.-")"
 echo "::debug::Branch name of destination branch is $branchName"
 
 git config user.name "Cookbook bot"
 git config user.email "bot@noreply.github.com"
 
 backportBranch() {
-	backportBranchName="backport-$branchName/$headSha"
+	backportBranchName="backport-$branchName/$headRef"
 	echo "::debug::Backport branch name to create is $backportBranchName"
 	echo "branchName=$backportBranchName" >> "$GITHUB_OUTPUT"
-	echo "targetBranch=$baseRef" >> "$GITHUB_OUTPUT"
+	echo "targetBranch=$branchName" >> "$GITHUB_OUTPUT"
 	
 	git branch "$backportBranchName" "$headSha"
 	
@@ -29,11 +37,13 @@ backportBranch() {
 
 	echo "Branch was created, ready for porting to other branch"
 
-	git rebase --onto "$branchName" "$baseSha" "$backportBranchName"
+	git rebase --onto "origin/$branchName" "$baseSha" "$backportBranchName"
 
 	echo "Rebasing was successful"
 
-	git push origin "$backportBranchName"
+	git checkout "$branchName"
+	git merge --ff-only "$backportBranchName"
+	git branch -d "$backportBranchName"
 }
 
 backportBranch
