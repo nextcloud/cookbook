cea1d2c5e29fc3723a79998e7beb5939fde52fea
Sebastian Fey <info@sebastianfey.de>
2023-12-12 09:40:18 +0100

    fix: Fix error when changing the update interval in the settings.
    Signed-off-by: Sebastian Fey <sebastian@arfinity.io>
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- src/components/Modals/SettingsDialog.vue
+++ src/components/Modals/SettingsDialog.vue
@@ -301,6 +301,7 @@ watch(
             return;
         }
         try {
+            if (newVal === '') return;
             await api.config.updateInterval.update(newVal);
             await store.dispatch('refreshConfig');
         } catch {
