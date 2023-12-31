6429f13722f1b9a0dd756f98ae8e89c10c5c387c
Sebastian Fey <info@sebastianfey.de>
2023-12-31 09:27:32 +0100

    feat: Add component for toggling between two icons.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/components/Utilities/ToggleIcon.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="checkbox">
+        <label :for="id" class="checkbox__label">
+            <component
+                :is="isChecked ? checkedIcon : uncheckedIcon"
+                v-bind="
+                    isChecked
+                        ? { ...iconProps, ...checkedIconProps }
+                        : { ...iconProps, ...uncheckedIconProps }
+                "
+            />
+        </label>
+        <input :id="id" v-model="isChecked" type="checkbox" @change="toggle" />
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from 'vue';
+
+const props = defineProps({
+    /**
+     * Unique identifier for the input element.
+     */
+    id: {
+        type: String,
+        default: () => Math.random().toString(36).substr(2, 9),
+    },
+    /**
+     * Icon component to be used for the checked state.
+     */
+    checkedIcon: {
+        type: Object,
+        default: () => {},
+    },
+    /**
+     * Icon component to be used for the unchecked state.
+     */
+    uncheckedIcon: {
+        type: Object,
+        default: () => {},
+    },
+    /**
+     * Properties passed the checked and unchecked icon. Will be merged with the more specific props for each state.
+     */
+    iconProps: {
+        type: Object,
+        default: () => {},
+    },
+    /**
+     * Properties passed to the checked icon. Will be merged with `iconProps`.
+     */
+    checkedIconProps: {
+        type: Object,
+        default: () => {},
+    },
+    /**
+     * Properties passed to the unchecked icon. Will be merged with `iconProps`.
+     */
+    uncheckedIconProps: {
+        type: Object,
+        default: () => {},
+    },
+    /**
+     * Checked state of the input.
+     */
+    value: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+});
+
+const emit = defineEmits(['check', 'uncheck', 'update']);
+
+/**
+ * Local value of the input.
+ * @type {import('vue').Ref<boolean>}
+ */
+const isChecked = ref(props.value);
+
+// Watch the model value and update local value on external updates.
+watch(
+    () => props.value,
+    (newValue) => {
+        isChecked.value = newValue;
+    },
+);
+
+/**
+ * Toggle the value on the input between `true` and `false`.
+ */
+function toggle() {
+    emit('update', isChecked.value);
+    if (isChecked.value) {
+        emit('check');
+    } else {
+        emit('uncheck');
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox {
+    cursor: pointer;
+}
+input[type='checkbox'] {
+    display: none;
+}
+.checkbox__label {
+    cursor: pointer;
+    display: flex;
+
+    span {
+        cursor: pointer;
+    }
+}
+</style>
