0a92cd801ec0dcb1e8d7b01496609345f4353cbb
Christian Wolf <github@christianwolf.email>
2024-01-10 20:46:34 +0100

    Ignore test files in fixtures of the CI tests
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 module.exports = {
     testEnvironment: 'node',
     moduleFileExtensions: ['js', 'vue'],
+    modulePaths: [
+        '<rootDir>/src/'
+    ],
+    modulePathIgnorePatterns: [
+        '<rootDir>/.github/'
+    ],
     transform: {
         '.*\\.js$': '<rootDir>/node_modules/babel-jest',
         '.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue2-jest',
