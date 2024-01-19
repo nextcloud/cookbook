b17cca28430087df01291106bf89874edd27ecd1
Sebastian Fey <info@sebastianfey.de>
2024-01-19 16:11:56 +0100

    test: Allow testing typescript with Jest
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 const babelConfig = require('@nextcloud/babel-config');
 
+// https://jestjs.io/docs/getting-started#using-typescript
+babelConfig.presets.push('@babel/preset-typescript');
+
 module.exports = babelConfig;
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 // Jest configuration
 module.exports = {
     testEnvironment: 'node',
-    moduleFileExtensions: ['js', 'vue'],
-    modulePaths: [
-        '<rootDir>/src/'
-    ],
-    modulePathIgnorePatterns: [
-        '<rootDir>/.github/'
-    ],
+    moduleFileExtensions: ['js', 'ts', 'vue'],
+    modulePaths: ['<rootDir>/src/'],
+    modulePathIgnorePatterns: ['<rootDir>/.github/'],
     transform: {
         '.*\\.js$': '<rootDir>/node_modules/babel-jest',
+        '.*\\.ts$': '<rootDir>/node_modules/babel-jest',
         '.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue2-jest',
     },
     transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
