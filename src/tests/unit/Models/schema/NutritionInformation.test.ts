5710085792be5c1548c097374838393b6d2946ad
Sebastian Fey <info@sebastianfey.de>
2024-01-21 11:48:13 +0100

    test(js): Add tests for `fromJSON` method of `NutritionInformation` and `QuantitativeValue`.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/tests/unit/Models/schema/NutritionInformation.test.ts
+++ src/tests/unit/Models/schema/NutritionInformation.test.ts
@@ -3,57 +3,136 @@ import NutritionInformation, {
 } from '../../../../js/Models/schema/NutritionInformation';
 
 describe('NutritionInformation', () => {
-	test('should set the @type property to "NutritionInformation"', () => {
-		const properties: NutritionInformationProperties = {};
+	describe('constructor', () => {
+		test('should set the @type property to "NutritionInformation"', () => {
+			const properties: NutritionInformationProperties = {};
 
-		const nutritionInfo = new NutritionInformation(properties);
+			const nutritionInfo = new NutritionInformation(properties);
 
-		expect(nutritionInfo['@type']).toBe('NutritionInformation');
+			expect(nutritionInfo['@type']).toBe('NutritionInformation');
+		});
+
+		test('should create an instance of NutritionInformation with specified properties', () => {
+			const properties: NutritionInformationProperties = {
+				calories: '100',
+				carbohydrateContent: '20',
+				proteinContent: '15',
+				servingSize: '1 cup',
+				sodiumContent: '200',
+			};
+
+			const nutritionInfo = new NutritionInformation(properties);
+
+			expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
+			expect(nutritionInfo.calories).toBe(properties.calories);
+			expect(nutritionInfo.carbohydrateContent).toBe(
+				properties.carbohydrateContent,
+			);
+			expect(nutritionInfo.cholesterolContent).toBeUndefined(); // Added test for cholesterolContent
+			expect(nutritionInfo.fatContent).toBeUndefined(); // Added test for fatContent
+			expect(nutritionInfo.fiberContent).toBeUndefined(); // Added test for fiberContent
+			expect(nutritionInfo.proteinContent).toBe(
+				properties.proteinContent,
+			);
+			expect(nutritionInfo.saturatedFatContent).toBeUndefined(); // Added test for saturatedFatContent
+			expect(nutritionInfo.servingSize).toBe(properties.servingSize);
+			expect(nutritionInfo.sodiumContent).toBe(properties.sodiumContent);
+			expect(nutritionInfo.sugarContent).toBeUndefined(); // Added test for sugarContent
+			expect(nutritionInfo.transFatContent).toBeUndefined(); // Added test for transFatContent
+			expect(nutritionInfo.unsaturatedFatContent).toBeUndefined(); // Added test for unsaturatedFatContent
+		});
+
+		test('should create an instance of NutritionInformation with all properties set to undefined', () => {
+			const nutritionInfo = new NutritionInformation();
+
+			expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
+			expect(nutritionInfo.calories).toBeUndefined();
+			expect(nutritionInfo.carbohydrateContent).toBeUndefined();
+			expect(nutritionInfo.cholesterolContent).toBeUndefined();
+			expect(nutritionInfo.fatContent).toBeUndefined();
+			expect(nutritionInfo.fiberContent).toBeUndefined();
+			expect(nutritionInfo.proteinContent).toBeUndefined();
+			expect(nutritionInfo.saturatedFatContent).toBeUndefined();
+			expect(nutritionInfo.servingSize).toBeUndefined();
+			expect(nutritionInfo.sodiumContent).toBeUndefined();
+			expect(nutritionInfo.sugarContent).toBeUndefined();
+			expect(nutritionInfo.transFatContent).toBeUndefined();
+			expect(nutritionInfo.unsaturatedFatContent).toBeUndefined();
+		});
 	});
 
-	test('should create an instance of NutritionInformation with specified properties', () => {
-		const properties: NutritionInformationProperties = {
-			calories: '100',
-			carbohydrateContent: '20',
-			proteinContent: '15',
-			servingSize: '1 cup',
-			sodiumContent: '200',
-		};
+	describe('fromJSON', () => {
+		it('should create a NutritionInformation instance from valid JSON', () => {
+			const validJSON =
+				'{"calories": "100", "carbohydrateContent": "20g", "cholesterolContent": "10mg", "fatContent": "5g", "fiberContent": "3g", "proteinContent": "8g", "saturatedFatContent": "2g", "servingSize": "1 cup", "sodiumContent": "300mg", "sugarContent": "5g", "transFatContent": "0g", "unsaturatedFatContent": "3g"}';
 
-		const nutritionInfo = new NutritionInformation(properties);
+			const nutritionInfo = NutritionInformation.fromJSON(validJSON);
 
-		expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
-		expect(nutritionInfo.calories).toBe(properties.calories);
-		expect(nutritionInfo.carbohydrateContent).toBe(
-			properties.carbohydrateContent,
-		);
-		expect(nutritionInfo.cholesterolContent).toBeUndefined(); // Added test for cholesterolContent
-		expect(nutritionInfo.fatContent).toBeUndefined(); // Added test for fatContent
-		expect(nutritionInfo.fiberContent).toBeUndefined(); // Added test for fiberContent
-		expect(nutritionInfo.proteinContent).toBe(properties.proteinContent);
-		expect(nutritionInfo.saturatedFatContent).toBeUndefined(); // Added test for saturatedFatContent
-		expect(nutritionInfo.servingSize).toBe(properties.servingSize);
-		expect(nutritionInfo.sodiumContent).toBe(properties.sodiumContent);
-		expect(nutritionInfo.sugarContent).toBeUndefined(); // Added test for sugarContent
-		expect(nutritionInfo.transFatContent).toBeUndefined(); // Added test for transFatContent
-		expect(nutritionInfo.unsaturatedFatContent).toBeUndefined(); // Added test for unsaturatedFatContent
-	});
+			expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
+			expect(nutritionInfo.calories).toEqual('100');
+			expect(nutritionInfo.carbohydrateContent).toEqual('20g');
+			expect(nutritionInfo.cholesterolContent).toEqual('10mg');
+			expect(nutritionInfo.fatContent).toEqual('5g');
+			expect(nutritionInfo.fiberContent).toEqual('3g');
+			expect(nutritionInfo.proteinContent).toEqual('8g');
+			expect(nutritionInfo.saturatedFatContent).toEqual('2g');
+			expect(nutritionInfo.servingSize).toEqual('1 cup');
+			expect(nutritionInfo.sodiumContent).toEqual('300mg');
+			expect(nutritionInfo.sugarContent).toEqual('5g');
+			expect(nutritionInfo.transFatContent).toEqual('0g');
+			expect(nutritionInfo.unsaturatedFatContent).toEqual('3g');
+		});
 
-	test('should create an instance of NutritionInformation with all properties set to undefined', () => {
-		const nutritionInfo = new NutritionInformation();
+		it('should create a NutritionInformation instance with missing properties from JSON', () => {
+			const validJSON =
+				'{"calories": "100", "fatContent": "5g", "proteinContent": "8g", "saturatedFatContent": "2g"}';
 
-		expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
-		expect(nutritionInfo.calories).toBeUndefined();
-		expect(nutritionInfo.carbohydrateContent).toBeUndefined();
-		expect(nutritionInfo.cholesterolContent).toBeUndefined();
-		expect(nutritionInfo.fatContent).toBeUndefined();
-		expect(nutritionInfo.fiberContent).toBeUndefined();
-		expect(nutritionInfo.proteinContent).toBeUndefined();
-		expect(nutritionInfo.saturatedFatContent).toBeUndefined();
-		expect(nutritionInfo.servingSize).toBeUndefined();
-		expect(nutritionInfo.sodiumContent).toBeUndefined();
-		expect(nutritionInfo.sugarContent).toBeUndefined();
-		expect(nutritionInfo.transFatContent).toBeUndefined();
-		expect(nutritionInfo.unsaturatedFatContent).toBeUndefined();
+			const nutritionInfo = NutritionInformation.fromJSON(validJSON);
+
+			expect(nutritionInfo).toBeInstanceOf(NutritionInformation);
+			expect(nutritionInfo.calories).toEqual('100');
+			expect(nutritionInfo.fatContent).toEqual('5g');
+			expect(nutritionInfo.proteinContent).toEqual('8g');
+			expect(nutritionInfo.saturatedFatContent).toEqual('2g');
+			// Other properties should be undefined
+			expect(nutritionInfo.carbohydrateContent).toBeUndefined();
+			expect(nutritionInfo.cholesterolContent).toBeUndefined();
+			expect(nutritionInfo.fiberContent).toBeUndefined();
+			expect(nutritionInfo.servingSize).toBeUndefined();
+			expect(nutritionInfo.sodiumContent).toBeUndefined();
+			expect(nutritionInfo.sugarContent).toBeUndefined();
+			expect(nutritionInfo.transFatContent).toBeUndefined();
+			expect(nutritionInfo.unsaturatedFatContent).toBeUndefined();
+		});
+
+		it('should throw an error for invalid JSON with non-string (number) property values', () => {
+			const invalidJSON =
+				'{"calories": 100, "fatContent": "5g", "proteinContent": "8g", "saturatedFatContent": "2g"}';
+
+			expect(() =>
+				NutritionInformation.fromJSON(invalidJSON),
+			).toThrowError(
+				'Invalid property value: "calories" must be a string',
+			);
+		});
+
+		it('should throw an error for invalid JSON with non-string (object) property values', () => {
+			const invalidJSON =
+				'{"calories": "100", "fatContent": {"value": "5g"}, "proteinContent": "8g", "saturatedFatContent": "2g"}';
+
+			expect(() =>
+				NutritionInformation.fromJSON(invalidJSON),
+			).toThrowError(
+				'Invalid property value: "fatContent" must be a string',
+			);
+		});
+
+		test('should throw an error for invalid JSON', () => {
+			const invalidJson = 'Invalid JSON string';
+
+			expect(() => NutritionInformation.fromJSON(invalidJson)).toThrow(
+				'Error mapping to "NutritionInformation". Received invalid JSON: "Invalid JSON string"',
+			);
+		});
 	});
 });
--- src/tests/unit/Models/schema/QuantitativeValue.test.ts
+++ src/tests/unit/Models/schema/QuantitativeValue.test.ts
@@ -1,24 +1,89 @@
 import QuantitativeValue from '../../../../js/Models/schema/QuantitativeValue';
 
 describe('QuantitativeValue', () => {
-	test('should set the @type property to "QuantitativeValue"', () => {
-		const quantitativeValue = new QuantitativeValue(15, 'meter');
-		expect(quantitativeValue['@type']).toBe('QuantitativeValue');
+	describe('constructor', () => {
+		test('should set the @type property to "QuantitativeValue"', () => {
+			const quantitativeValue = new QuantitativeValue(15, 'meter');
+			expect(quantitativeValue['@type']).toBe('QuantitativeValue');
+		});
+
+		test('should create an instance of QuantitativeValue with unitCode undefined', () => {
+			const quantitativeValue = new QuantitativeValue(10, 'cup');
+			expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
+			expect(quantitativeValue.value).toBe(10);
+			expect(quantitativeValue.unitText).toBe('cup');
+			expect(quantitativeValue.unitCode).toBeUndefined();
+		});
+
+		test('should create an instance of QuantitativeValue with specified unitCode', () => {
+			const quantitativeValue = new QuantitativeValue(
+				5,
+				'kilogram',
+				'KGM',
+			);
+			expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
+			expect(quantitativeValue.value).toBe(5);
+			expect(quantitativeValue.unitText).toBe('kilogram');
+			expect(quantitativeValue.unitCode).toBe('KGM');
+		});
 	});
 
-	test('should create an instance of QuantitativeValue with unitCode undefined', () => {
-		const quantitativeValue = new QuantitativeValue(10, 'cup');
-		expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
-		expect(quantitativeValue.value).toBe(10);
-		expect(quantitativeValue.unitText).toBe('cup');
-		expect(quantitativeValue.unitCode).toBeUndefined();
-	});
+	describe('fromJSON', () => {
+		it('should create a QuantitativeValue instance from a valid JSON string', () => {
+			const jsonString =
+				'{"value": 250, "unitText": "grams", "unitCode": "G"}';
+			const quantitativeValue = QuantitativeValue.fromJSON(jsonString);
+			expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
+			expect(quantitativeValue.value).toBe(250);
+			expect(quantitativeValue.unitText).toBe('grams');
+			expect(quantitativeValue.unitCode).toBe('G');
+		});
 
-	test('should create an instance of QuantitativeValue with specified unitCode', () => {
-		const quantitativeValue = new QuantitativeValue(5, 'kilogram', 'KGM');
-		expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
-		expect(quantitativeValue.value).toBe(5);
-		expect(quantitativeValue.unitText).toBe('kilogram');
-		expect(quantitativeValue.unitCode).toBe('KGM');
+		it('should create a QuantitativeValue instance from a valid JSON object', () => {
+			const jsonObject = { value: 2, unitText: 'cups', unitCode: 'CUP' };
+			const quantitativeValue = QuantitativeValue.fromJSON(jsonObject);
+			expect(quantitativeValue).toBeInstanceOf(QuantitativeValue);
+			expect(quantitativeValue.value).toBe(2);
+			expect(quantitativeValue.unitText).toBe('cups');
+			expect(quantitativeValue.unitCode).toBe('CUP');
+		});
+
+		it('should throw an error for invalid JSON string', () => {
+			const invalidJsonString =
+				'{"value": "invalid", "unitText": "grams", "unitCode": "G"}';
+			expect(() => QuantitativeValue.fromJSON(invalidJsonString)).toThrow(
+				'Error mapping QuantitativeValue \'value\'. Expected integer number but received non-parsable string "invalid".',
+			);
+		});
+
+		it('should throw an error for missing "value" property', () => {
+			const jsonString = '{"unitText": "grams", "unitCode": "G"}';
+			expect(() => QuantitativeValue.fromJSON(jsonString)).toThrow(
+				'Error mapping QuantitativeValue \'value\'. Expected integer number but received "undefined".',
+			);
+		});
+
+		it('should throw an error for missing "unitText" property', () => {
+			const jsonString = '{"value": 250, "unitCode": "G"}';
+			expect(() => QuantitativeValue.fromJSON(jsonString)).toThrow(
+				'Error mapping QuantitativeValue \'value\'. Expected string but received "undefined".',
+			);
+		});
+
+		it('should throw an error for invalid "unitCode" property type', () => {
+			const jsonString =
+				'{"value": 250, "unitText": "grams", "unitCode": 123}';
+			expect(() => QuantitativeValue.fromJSON(jsonString)).toThrow(
+				'Error mapping QuantitativeValue \'value\'. Expected string but received "number".',
+			);
+		});
+
+		test('should throw an error for invalid JSON', () => {
+			const invalidJson = 'Invalid JSON string';
+
+			expect(() => QuantitativeValue.fromJSON(invalidJson)).toThrow(
+				'Error mapping to "QuantitativeValue". Received invalid JSON: "Invalid JSON string"',
+			);
+		});
 	});
 });
