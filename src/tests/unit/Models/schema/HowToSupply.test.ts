6723100aa1a0aadd9578fb1e85327f56dbb50a1f
Sebastian Fey <info@sebastianfey.de>
2024-01-21 11:48:38 +0100

    test(js): Add tests for `fromJSON` method of `HowToSupply` and `HowToTool`.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/tests/unit/Models/schema/HowToSupply.test.ts
+++ src/tests/unit/Models/schema/HowToSupply.test.ts
@@ -1,49 +1,127 @@
-import HowToSupply from '../../../../js/Models/schema/HowToSupply';
+import HowToSupply from 'cookbook/js/Models/schema/HowToSupply';
 
 describe('HowToSupply', () => {
-	test('should set the @type property to "HowToSupply"', () => {
-		const howToSupply = new HowToSupply('Ingredient');
-		expect(howToSupply['@type']).toBe('HowToSupply');
-	});
+	describe('constructor', () => {
+		test('should set the @type property to "HowToSupply"', () => {
+			const howToSupply = new HowToSupply('Ingredient');
+			expect(howToSupply['@type']).toBe('HowToSupply');
+		});
 
-	test('should create an instance of HowToSupply with optional properties undefined', () => {
-		const howToSupply = new HowToSupply('Ingredient');
-		expect(howToSupply).toBeInstanceOf(HowToSupply);
-		expect(howToSupply.name).toBe('Ingredient');
-		expect(howToSupply.identifier).toBeUndefined();
-		expect(howToSupply.description).toBeUndefined();
-		expect(howToSupply.requiredQuantity).toBeUndefined();
-	});
+		test('should create an instance of HowToSupply with optional properties undefined', () => {
+			const howToSupply = new HowToSupply('Ingredient');
+			expect(howToSupply).toBeInstanceOf(HowToSupply);
+			expect(howToSupply.name).toBe('Ingredient');
+			expect(howToSupply.identifier).toBeUndefined();
+			expect(howToSupply.description).toBeUndefined();
+			expect(howToSupply.requiredQuantity).toBeUndefined();
+		});
 
-	test('should create an instance of HowToSupply with some optional properties defined and some undefined', () => {
-		const howToSupply = new HowToSupply('Ingredient', 'IGD123');
-		expect(howToSupply).toBeInstanceOf(HowToSupply);
-		expect(howToSupply.name).toBe('Ingredient');
-		expect(howToSupply.identifier).toBe('IGD123');
-		expect(howToSupply.description).toBeUndefined();
-		expect(howToSupply.requiredQuantity).toBeUndefined();
-	});
+		test('should create an instance of HowToSupply with some optional properties defined and some undefined', () => {
+			const howToSupply = new HowToSupply('Ingredient', 'IGD123');
+			expect(howToSupply).toBeInstanceOf(HowToSupply);
+			expect(howToSupply.name).toBe('Ingredient');
+			expect(howToSupply.identifier).toBe('IGD123');
+			expect(howToSupply.description).toBeUndefined();
+			expect(howToSupply.requiredQuantity).toBeUndefined();
+		});
 
-	test('should create an instance of HowToSupply with all properties set', () => {
-		const howToSupply = new HowToSupply(
-			'Flour',
-			'FLR123',
-			'High-quality flour',
-			{
+		test('should create an instance of HowToSupply with all properties set', () => {
+			const howToSupply = new HowToSupply(
+				'Flour',
+				'FLR123',
+				'High-quality flour',
+				{
+					value: 2,
+					unitText: 'cup',
+					unitCode: undefined,
+				},
+			);
+
+			expect(howToSupply).toBeInstanceOf(HowToSupply);
+			expect(howToSupply.name).toBe('Flour');
+			expect(howToSupply.identifier).toBe('FLR123');
+			expect(howToSupply.description).toBe('High-quality flour');
+			expect(howToSupply.requiredQuantity).toEqual({
 				value: 2,
 				unitText: 'cup',
 				unitCode: undefined,
-			},
-		);
+			});
+		});
+	});
 
-		expect(howToSupply).toBeInstanceOf(HowToSupply);
-		expect(howToSupply.name).toBe('Flour');
-		expect(howToSupply.identifier).toBe('FLR123');
-		expect(howToSupply.description).toBe('High-quality flour');
-		expect(howToSupply.requiredQuantity).toEqual({
-			value: 2,
-			unitText: 'cup',
-			unitCode: undefined,
+	describe('parseJSON', () => {
+		const createValidJSON = () => ({
+			name: 'Flour',
+			identifier: 'FLR123',
+			description: 'High-quality flour',
+			requiredQuantity: {
+				value: 1,
+				unitText: 'cup',
+			},
+		});
+
+		it('should create an instance from valid JSON', () => {
+			const validJSON = createValidJSON();
+			const tool = HowToSupply.fromJSON(validJSON);
+			expect(tool).toBeInstanceOf(HowToSupply);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBe(validJSON.identifier);
+			expect(tool.description).toBe(validJSON.description);
+			expect(tool.requiredQuantity).toBeDefined();
+			// Add more specific checks for QuantitativeValue if needed
+		});
+
+		it('should create an instance from valid JSON string', () => {
+			const validJSON = createValidJSON();
+			const tool = HowToSupply.fromJSON(JSON.stringify(validJSON));
+			expect(tool).toBeInstanceOf(HowToSupply);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBe(validJSON.identifier);
+			expect(tool.description).toBe(validJSON.description);
+			expect(tool.requiredQuantity).toBeDefined();
+			// Add more specific checks for QuantitativeValue if needed
+		});
+
+		it('should handle missing optional properties', () => {
+			const validJSON = { name: 'Knife' };
+			const tool = HowToSupply.fromJSON(validJSON);
+			expect(tool).toBeInstanceOf(HowToSupply);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBeUndefined();
+			expect(tool.description).toBeUndefined();
+			expect(tool.requiredQuantity).toBeUndefined();
+		});
+
+		it('should throw an error for invalid JSON', () => {
+			const invalidJSON = { name: 123 }; // 'name' should be a string
+			expect(() => HowToSupply.fromJSON(invalidJSON)).toThrow();
+		});
+
+		it('should throw an error for invalid JSON string', () => {
+			const invalidJSONString = '{"name": 123}'; // 'name' should be a string
+			expect(() => HowToSupply.fromJSON(invalidJSONString)).toThrow();
+		});
+
+		it('should throw an error for invalid JSON with missing name property', () => {
+			const invalidJSON = { prop: 123 }; // 'name' is missing
+			expect(() => HowToSupply.fromJSON(invalidJSON)).toThrow(
+				'Error mapping HowToSupply \'name\'. Expected string but received "undefined".',
+			);
+		});
+
+		it('should throw an error for invalid JSON string with missing name property', () => {
+			const invalidJSONString = '{"prop": 123}'; // 'name' is missing
+			expect(() => HowToSupply.fromJSON(invalidJSONString)).toThrow(
+				'Error mapping HowToSupply \'name\'. Expected string but received "undefined".',
+			);
+		});
+
+		test('should throw an error for invalid JSON', () => {
+			const invalidJson = 'Invalid JSON string';
+
+			expect(() => HowToSupply.fromJSON(invalidJson)).toThrow(
+				'Error mapping to "HowToSupply". Received invalid JSON: "Invalid JSON string"',
+			);
 		});
 	});
 });
--- src/tests/unit/Models/schema/HowToTool.test.ts
+++ src/tests/unit/Models/schema/HowToTool.test.ts
@@ -1,44 +1,127 @@
 import HowToTool from '../../../../js/Models/schema/HowToTool';
 
 describe('HowToTool', () => {
-	test('should set the @type property to "HowToTool"', () => {
-		const howToTool = new HowToTool('ToolA');
-		expect(howToTool['@type']).toBe('HowToTool');
-	});
-
-	test('should create an instance of HowToTool with optional properties undefined', () => {
-		const howToTool = new HowToTool('ToolA');
-		expect(howToTool).toBeInstanceOf(HowToTool);
-		expect(howToTool.name).toBe('ToolA');
-		expect(howToTool.identifier).toBeUndefined();
-		expect(howToTool.description).toBeUndefined();
-		expect(howToTool.requiredQuantity).toBeUndefined();
-	});
-
-	test('should create an instance of HowToTool with some optional properties defined and some undefined', () => {
-		const howToTool = new HowToTool('ToolA', 'TA123');
-		expect(howToTool).toBeInstanceOf(HowToTool);
-		expect(howToTool.name).toBe('ToolA');
-		expect(howToTool.identifier).toBe('TA123');
-		expect(howToTool.description).toBeUndefined();
-		expect(howToTool.requiredQuantity).toBeUndefined();
-	});
-
-	test('should create an instance of HowToTool with all properties set', () => {
-		const howToTool = new HowToTool('ToolB', 'TB123', 'High-quality tool', {
-			value: 2,
-			unitText: 'pcs',
-			unitCode: undefined,
+	describe('constructor', () => {
+		test('should set the @type property to "HowToTool"', () => {
+			const howToTool = new HowToTool('ToolA');
+			expect(howToTool['@type']).toBe('HowToTool');
 		});
 
-		expect(howToTool).toBeInstanceOf(HowToTool);
-		expect(howToTool.name).toBe('ToolB');
-		expect(howToTool.identifier).toBe('TB123');
-		expect(howToTool.description).toBe('High-quality tool');
-		expect(howToTool.requiredQuantity).toEqual({
-			value: 2,
-			unitText: 'pcs',
-			unitCode: undefined,
+		test('should create an instance of HowToTool with optional properties undefined', () => {
+			const howToTool = new HowToTool('ToolA');
+			expect(howToTool).toBeInstanceOf(HowToTool);
+			expect(howToTool.name).toBe('ToolA');
+			expect(howToTool.identifier).toBeUndefined();
+			expect(howToTool.description).toBeUndefined();
+			expect(howToTool.requiredQuantity).toBeUndefined();
+		});
+
+		test('should create an instance of HowToTool with some optional properties defined and some undefined', () => {
+			const howToTool = new HowToTool('ToolA', 'TA123');
+			expect(howToTool).toBeInstanceOf(HowToTool);
+			expect(howToTool.name).toBe('ToolA');
+			expect(howToTool.identifier).toBe('TA123');
+			expect(howToTool.description).toBeUndefined();
+			expect(howToTool.requiredQuantity).toBeUndefined();
+		});
+
+		test('should create an instance of HowToTool with all properties set', () => {
+			const howToTool = new HowToTool(
+				'ToolB',
+				'TB123',
+				'High-quality tool',
+				{
+					value: 2,
+					unitText: 'pcs',
+					unitCode: undefined,
+				},
+			);
+
+			expect(howToTool).toBeInstanceOf(HowToTool);
+			expect(howToTool.name).toBe('ToolB');
+			expect(howToTool.identifier).toBe('TB123');
+			expect(howToTool.description).toBe('High-quality tool');
+			expect(howToTool.requiredQuantity).toEqual({
+				value: 2,
+				unitText: 'pcs',
+				unitCode: undefined,
+			});
+		});
+	});
+
+	describe('parseJSON', () => {
+		const createValidJSON = () => ({
+			name: 'Knife',
+			identifier: 'tool123',
+			description: 'A sharp cutting tool',
+			requiredQuantity: {
+				value: 1,
+				unitText: 'unit',
+			},
+		});
+
+		it('should create an instance from valid JSON', () => {
+			const validJSON = createValidJSON();
+			const tool = HowToTool.fromJSON(validJSON);
+			expect(tool).toBeInstanceOf(HowToTool);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBe(validJSON.identifier);
+			expect(tool.description).toBe(validJSON.description);
+			expect(tool.requiredQuantity).toBeDefined();
+			// Add more specific checks for QuantitativeValue if needed
+		});
+
+		it('should create an instance from valid JSON string', () => {
+			const validJSON = createValidJSON();
+			const tool = HowToTool.fromJSON(JSON.stringify(validJSON));
+			expect(tool).toBeInstanceOf(HowToTool);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBe(validJSON.identifier);
+			expect(tool.description).toBe(validJSON.description);
+			expect(tool.requiredQuantity).toBeDefined();
+			// Add more specific checks for QuantitativeValue if needed
+		});
+
+		it('should handle missing optional properties', () => {
+			const validJSON = { name: 'Knife' };
+			const tool = HowToTool.fromJSON(validJSON);
+			expect(tool).toBeInstanceOf(HowToTool);
+			expect(tool.name).toBe(validJSON.name);
+			expect(tool.identifier).toBeUndefined();
+			expect(tool.description).toBeUndefined();
+			expect(tool.requiredQuantity).toBeUndefined();
+		});
+
+		it('should throw an error for invalid JSON', () => {
+			const invalidJSON = { name: 123 }; // 'name' should be a string
+			expect(() => HowToTool.fromJSON(invalidJSON)).toThrow();
+		});
+
+		it('should throw an error for invalid JSON string', () => {
+			const invalidJSONString = '{"name": 123}'; // 'name' should be a string
+			expect(() => HowToTool.fromJSON(invalidJSONString)).toThrow();
+		});
+
+		it('should throw an error for invalid JSON with missing name property', () => {
+			const invalidJSON = { prop: 123 }; // 'name' is missing
+			expect(() => HowToTool.fromJSON(invalidJSON)).toThrow(
+				'Error mapping HowToTool \'name\'. Expected string but received "undefined".',
+			);
+		});
+
+		it('should throw an error for invalid JSON string with missing name property', () => {
+			const invalidJSONString = '{"prop": 123}'; // 'name' is missing
+			expect(() => HowToTool.fromJSON(invalidJSONString)).toThrow(
+				'Error mapping HowToTool \'name\'. Expected string but received "undefined".',
+			);
+		});
+
+		test('should throw an error for invalid JSON', () => {
+			const invalidJson = 'Invalid JSON string';
+
+			expect(() => HowToTool.fromJSON(invalidJson)).toThrow(
+				'Error mapping to "HowToTool". Received invalid JSON: "Invalid JSON string"',
+			);
 		});
 	});
 });
