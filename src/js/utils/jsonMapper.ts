62cdd1ac5c9b56be8e9aa11e95fc2fb677e3bddb
Sebastian Fey <info@sebastianfey.de>
2024-01-31 17:00:25 +0100

    feat: Add option to split comma-separated string to array in `jsonMapper` `mapStringOrStringArray()`
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/js/utils/jsonMapper.ts
+++ src/js/utils/jsonMapper.ts
@@ -56,6 +56,8 @@ export function mapInteger(
  * @param value The value to be mapped.
  * @param targetName The name of the target property. Only used for error message.
  * @param allowNullOrUndefined If true `null` or `undefined` will be immediately returned. If false, an exception will be thrown.
+ * @param treatStringAsCommaSeparatedList If true and a single string is passed, the string is treated as a
+ * 	comma-separated list and mapped to an array.
  * @throws JsonMappingException Thrown if `value` cannot be mapped to a string or an array of strings.
  * @returns Either the value as a string or an array of strings if mapping was successful or null/undefined if the
  * value was null/undefined.
@@ -64,6 +66,7 @@ export function mapStringOrStringArray(
 	value: unknown,
 	targetName: string = '',
 	allowNullOrUndefined: boolean = false,
+	treatStringAsCommaSeparatedList = false,
 ): string | string[] | null | undefined {
 	if (value === undefined || value === null) {
 		// Return null or undefined immediately
@@ -91,7 +94,16 @@ export function mapStringOrStringArray(
 	}
 
 	// `value` is a string, return.
-	return value;
+	if (!treatStringAsCommaSeparatedList) return value;
+
+	return (
+		value
+			.split(',')
+			.map((str) => str.trim())
+			// Remove any empty strings
+			// If empty string, split will create an array of a single empty string
+			.filter((str) => str !== '')
+	);
 }
 
 /**
