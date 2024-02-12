9de47df0a9871172b64057f7cbddc82d7278e721
Sebastian Fey <info@sebastianfey.de>
2024-02-12 08:38:35 +0100

    test: Add tests for adjustToInteger method
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/tests/unit/helper.test.ts
+++ src/tests/unit/helper.test.ts
@@ -1,4 +1,38 @@
-import { asArray, asCleanedArray } from 'cookbook/js/helper';
+import { adjustToInteger, asArray, asCleanedArray } from 'cookbook/js/helper';
+
+// adjustToIntegerToInteger() tests
+describe('adjustToInteger function', () => {
+	it('should increase the value by 1 when step is 1', () => {
+		expect(adjustToInteger(1, 1)).toBe(2);
+		expect(adjustToInteger(1.5, 1)).toBe(2);
+	});
+
+	it('should increase the value by 2 when step is 2', () => {
+		expect(adjustToInteger(1, 2)).toBe(3);
+		expect(adjustToInteger(1.5, 2)).toBe(3);
+	});
+
+	it('should decrease the value by 1 when step is -1', () => {
+		expect(adjustToInteger(2, -1)).toBe(1);
+		expect(adjustToInteger(2.5, -1)).toBe(2);
+		expect(adjustToInteger(1, -1)).toBe(1);
+		expect(adjustToInteger(0.5, -1)).toBe(0.5);
+	});
+
+	it('should decrease the value by 2 when step is -2', () => {
+		expect(adjustToInteger(3, -2)).toBe(1);
+		expect(adjustToInteger(3.5, -2)).toBe(2);
+		expect(adjustToInteger(1.5, -2)).toBe(1);
+		expect(adjustToInteger(0.5, -2)).toBe(0.5);
+	});
+
+	it('should handle values between 0 and 1 correctly with negative step', () => {
+		expect(adjustToInteger(0.5, -1)).toBe(0.5);
+		expect(adjustToInteger(0.5, -2)).toBe(0.5);
+		expect(adjustToInteger(0.3, -1)).toBe(0.3);
+		expect(adjustToInteger(0.3, -2)).toBe(0.3);
+	});
+});
 
 // asArray() tests
 describe('asArray', () => {
