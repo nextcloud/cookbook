d69c788cc2f9b66c944bdcc95a5357791fd219df
Sebastian Fey <info@sebastianfey.de>
2024-03-17 19:01:28 +0100

    feat(recipe-view): Add organizational items section with category to sidebar
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/components/RecipeView/Sidebar/RecipeDetailsTab.vue
+++ src/components/RecipeView/Sidebar/RecipeDetailsTab.vue
@@ -3,9 +3,23 @@
         <template #icon>
             <DetailsIcon :size="20" />
         </template>
-        <div class="recipe-category">
-            Category: {{ recipe !== null ? recipe.recipeCategory : '' }}
-        </div>
+        <section class="organizational">
+            <h3 class="mt-0">{{ t('cookbook', 'Organizational') }}</h3>
+            <div class="section-content recipe-category mb-4">
+                <span>{{ t('cookbook', 'Category') }}:</span>
+
+                <span
+                    v-if="recipe?.recipeCategory"
+                    class="inline-block border-1 border-solid rounded-full px-2.5 py-0.5"
+                    >{{ recipe.recipeCategory }}</span
+                >
+                <span v-else> {{ 't("cookbook", "Uncategorized")' }}</span>
+            </div>
+        </section>
+        <section v-if="showNutritionData" class="nutrition p-4 rounded">
+            <h3 class="mt-0">{{ t('cookbook', 'Nutrition Information') }}</h3>
+            <RecipeNutritionInformation :nutrition="recipe.nutrition" />
+        </section>
     </NcAppSidebarTab>
 </template>
 
@@ -21,3 +35,18 @@ defineProps({
     },
 });
 </script>
+
+<style lang="scss">
+section {
+    margin-bottom: 1.5rem;
+
+    h3 {
+        margin-bottom: 6px;
+        font-weight: bolder;
+    }
+
+    .section-content {
+        margin-left: 1rem;
+    }
+}
+</style>
