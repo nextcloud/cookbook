4a6aff59525364f8db544fd27ffe49a2dbfecb96
Sebastian Fey <info@sebastianfey.de>
2024-04-05 21:10:47 +0200

    feat(sheet): Add sheet component and rearrange SCSS styles files
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/components/Utilities/CbSheet.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        class="sheet"
+        :class="[
+            elevation > 0 && `elevation-${elevation}`,
+            { 'sheet--rounded': rounded },
+        ]"
+    >
+        <slot />
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    elevation: {
+        type: Number,
+        default: 0,
+    },
+    rounded: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<style scoped lang="scss">
+.sheet--rounded {
+    border-radius: var(--border-radius-large);
+}
+</style>
