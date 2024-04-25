6c8d6371c8dfc6a481fbbbd0e5646bc5ad00de9f
Sebastian Fey <info@sebastianfey.de>
2024-04-25 10:40:36 +0200

    test(utils): Add test for utility for removing duplicates from array in frontend.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/tests/unit/utils/removeDuplicates.test.ts
@@ -0,0 +1,89 @@
+import { removeDuplicatesInNestedStringArray } from 'cookbook/js/utils/removeDuplicates';
+
+describe('removeDuplicatesInNestedStringArray', () => {
+	test('should remove duplicate string arrays', () => {
+		const input = [
+			['apple', 'banana'],
+			['banana', 'apple'],
+			['cherry', 'date'],
+			['apple', 'banana'],
+		];
+		const expectedOutput = [
+			['apple', 'banana'],
+			['cherry', 'date'],
+		];
+
+		const result = removeDuplicatesInNestedStringArray(input);
+
+		// Check if the result has the expected length
+		expect(result.length).toBe(expectedOutput.length);
+
+		// Check if each element in the result matches the expected output
+		expectedOutput.forEach((expectedArray, index) => {
+			expect(result[index]).toEqual(
+				expect.arrayContaining(expectedArray),
+			);
+		});
+	});
+
+	test('should not remove non-duplicate string arrays', () => {
+		const input = [
+			['apple', 'banana'],
+			['cherry', 'date'],
+		];
+		const expectedOutput = [
+			['apple', 'banana'],
+			['cherry', 'date'],
+		];
+
+		const result = removeDuplicatesInNestedStringArray(input);
+
+		// Check if the result has the expected length
+		expect(result.length).toBe(expectedOutput.length);
+
+		// Check if each element in the result matches the expected output
+		expectedOutput.forEach((expectedArray, index) => {
+			expect(result[index]).toEqual(
+				expect.arrayContaining(expectedArray),
+			);
+		});
+	});
+
+	test('should handle empty array input', () => {
+		const input: string[][] = [];
+		const expectedOutput: string[][] = [];
+
+		const result = removeDuplicatesInNestedStringArray(input);
+
+		// Check if the result is an empty array
+		expect(result).toEqual(expectedOutput);
+	});
+
+	test('should handle inner arrays with varying numbers of strings', () => {
+		const input = [
+			['apple', 'banana', 'cherry'],
+			['apple', 'banana'],
+			['banana', 'apple'],
+			['apple', 'banana', 'cherry', 'date'],
+			['apple', 'banana', 'cherry'],
+		];
+		const expectedOutput = [
+			['apple', 'banana', 'cherry'],
+			['apple', 'banana'],
+			['apple', 'banana', 'cherry', 'date'],
+		];
+
+		const result = removeDuplicatesInNestedStringArray(input);
+		console.log(result);
+
+		// Check if the result has the expected length
+		expect(result.length).toBe(expectedOutput.length);
+
+		// Check if each element in the result matches the expected output
+		expectedOutput.forEach((expectedArray, index) => {
+			expect(result[index]).toEqual(
+				expect.arrayContaining(expectedArray),
+			);
+		});
+	});
+});
