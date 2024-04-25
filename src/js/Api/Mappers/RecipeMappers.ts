b2f4d7c9262a20806c1cf64c6e013d3bdb2bfeaf
Sebastian Fey <info@sebastianfey.de>
2024-04-25 11:01:24 +0200

    feat(api mapping): Add function for mapping category response to `RecipeMapper` and account for different styles of responses regarding the recipe identifier.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/js/Api/Mappers/RecipeMappers.ts
+++ src/js/Api/Mappers/RecipeMappers.ts
@@ -3,6 +3,36 @@ import { mapStringOrStringArray } from 'cookbook/js/utils/jsonMapper';
 
 export function mapApiRecipeResponseToRecipe(recipeDTO: {
 	id: string;
+	recipe_id: string;
+	identifier: string;
+	keywords: string | string[];
+	category: string | string[];
+}): Recipe {
+	// Create copy
+	const recipe = JSON.parse(JSON.stringify(recipeDTO));
+
+	// The cookbook API returns the `recipeCategory` property as `category`
+	recipe.recipeCategory = recipeDTO.category;
+
+	// TODO This should be unified some time (when the backend returns consistent responses ;)
+	// The cookbook API returns the `identifier` property as `id`
+	if (recipeDTO.id) recipe.identifier = recipeDTO.id;
+	else if (recipeDTO.recipe_id) recipe.identifier = recipeDTO.recipe_id;
+
+	// The cookbook API currently returns the `keywords` property as a comma-separated list instead of an array
+	const keywords = mapStringOrStringArray(
+		recipeDTO.keywords,
+		"Recipe 'keywords'",
+		true,
+		true,
+	);
+	recipe.keywords = keywords || [];
+
+	return Recipe.fromJSON(recipe);
+}
+
+export function mapApiRecipeByCategoryResponseToRecipe(recipeDTO: {
+	recipe_id: string;
 	identifier: string;
 	keywords: string | string[];
 	category: string | string[];
@@ -14,7 +44,7 @@ export function mapApiRecipeResponseToRecipe(recipeDTO: {
 	recipe.recipeCategory = recipeDTO.category;
 
 	// The cookbook API returns the `identifier` property as `id`
-	recipe.identifier = recipeDTO.id;
+	recipe.identifier = recipeDTO.recipe_id;
 	// The cookbook API returns the `keywords` property as a comma-separated list instead of an array
 	const keywords = mapStringOrStringArray(
 		recipeDTO.keywords,
