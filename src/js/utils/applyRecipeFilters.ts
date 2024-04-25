a3855eec73827a1d24dba389f0b96f0f8d4d8d87
Sebastian Fey <info@sebastianfey.de>
2024-04-25 10:50:42 +0200

    refactor(filtering): Add typescript annotations to  `applyRecipeFilters`
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

--- src/js/utils/applyRecipeFilters.ts
+++ src/js/utils/applyRecipeFilters.ts
@@ -1,13 +1,19 @@
+import { Recipe } from 'cookbook/js/Models/schema';
+import { RecipeFilter } from 'cookbook/js/RecipeFilters';
+
 /**
  * Function to apply a set of filters on an array of recipes.
  * @param {Object[]} recipes - The array of recipe objects to be filtered.
  * @param {RecipeFilter[]} filters - The array of filters to apply.
  * @returns {Object[]} The filtered array of recipes.
  */
-function applyRecipeFilters(recipes, filters) {
-    return recipes.filter((recipe) =>
-        filters.every((filter) => filter.filter(recipe)),
-    );
+function applyRecipeFilters(
+	recipes: Recipe[],
+	filters: RecipeFilter[],
+): Recipe[] {
+	return recipes.filter((recipe) =>
+		filters.every((filter) => filter.filter(recipe)),
+	);
 }
 
 export default applyRecipeFilters;
