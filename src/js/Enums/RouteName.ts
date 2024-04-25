a50a59700e265e665eaa27a9b26c2c451e584aae
Sebastian Fey <info@sebastianfey.de>
2024-04-25 10:56:16 +0200

    feat(routing): Add new `RouteName` enum for specifying the name of routes in a typesafe way.
    Signed-off-by: Sebastian Fey <info@sebastianfey.de>

---
+++ src/js/Enums/RouteName.ts
@@ -0,0 +1,15 @@
+/**
+ * Name of a route.
+ */
+enum RouteName {
+	SearchRecipesByCategory = 'search-category',
+	ShowRecipeInCategory = 'search-category__recipe-view',
+	SearchRecipesByName = 'search-name',
+	ShowRecipeInNames = 'search-name__recipe-view',
+	SearchRecipesByAnything = 'search-general',
+	ShowRecipeInGeneralSearch = 'search-general__recipe-view',
+	SearchRecipesByTags = 'search-tags',
+	ShowRecipeInTags = 'search-tags__recipe-view',
+}
+
+export default RouteName;
