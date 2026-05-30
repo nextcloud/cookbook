472fc8ab589d09a138ff5f1d3fce0455672e7e7d
Christian Wolf <github@christianwolf.email>
2026-05-30 16:55:26 +0200

    Migrate basic modal dialog implementation
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- src/composables/useCookbookDialogs/index.ts
+++ src/composables/useCookbookDialogs/index.ts
@@ -5,12 +5,16 @@ const resolvePromise = ref<null | ((value: any) => void)>(null);
 const buttons = ref({});
 const title = ref('');
 const message = ref('');
+const allowClose = ref(true);
+let nextClosingValue = null;
 
 export function useCookbookDialogs() {
-    function show(_title: string, _message: string, _buttons = {}) {
+    function show(_title: string, _message: string, _allowClose: boolean = true, _buttons = {}) {
         title.value = _title;
         message.value = _message;
+        allowClose.value = _allowClose;
         buttons.value = _buttons;
+        nextClosingValue = null;
         isDialogOpen.value = true;
         return new Promise((resolve) => {
             resolvePromise.value = resolve;
@@ -18,14 +22,19 @@ export function useCookbookDialogs() {
     }
     function close() {
         isDialogOpen.value = false;
-        resolvePromise.value?.(null);
+        resolvePromise.value?.(nextClosingValue);
+    }
+    function setClosingValue(value: any) {
+        nextClosingValue = value;
     }
     return {
         isDialogOpen,
         buttons,
         show,
         close,
+        setClosingValue,
         dialogTitle: title,
         dialogMessage: message,
+        dialogAllowClose: allowClose,
     };
 }
