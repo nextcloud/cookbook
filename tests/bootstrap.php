8c386cd693f06af680f8be4fff8a68da5c1616da
Côme Chilliet <come.chilliet@nextcloud.com>
2026-09-23 12:00:23 +0200

    fix: Remove legacy method calls from tests/bootstrap.php
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 // SPDX-FileCopyrightText: 2026 Nextcloud cookbook contributors
-//
 // SPDX-License-Identifier: AGPL-3.0-only OR AGPL-3.0-or-later
 
-require_once __DIR__ . '/bootstrap_helper.php';
+use OCP\App\IAppManager;
+use OCP\Server;
 
-require_once __DIR__ . '/../../../tests/bootstrap.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+define('PHPUNIT_RUN', 1);
 
-// Fix for "Autoload path not allowed: .../cookbook/tests/testcase.php"
-\OC_App::loadApp('cookbook');
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('cookbook');
