44b5e385170b1904bb73bbd690ce3c77f477b61d
Christian Wolf <github@christianwolf.email>
2026-09-24 14:58:38 +0200

    build: allow to use HMR on vite build
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+```js
 // SPDX-FileCopyrightText: 2026 Nextcloud cookbook contributors
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { createAppConfig } from '@nextcloud/vite-config'
 import { join, resolve } from 'path'
-import { visualizer } from "rollup-plugin-visualizer"
+import { loadEnv } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 const customPlugins = []
 
@@ -17,25 +19,35 @@ if (process.env.ENABLE_BUNDLE_ANALYZER === 'true') {
 	}))
 }
 
-export default createAppConfig(
-	{
-		main: resolve(join('src', 'main.ts')),
-		guest: resolve(join('src', 'guest.ts')),
-	}, {
-		createEmptyCSSEntryPoints: true,
-		extractLicenseInformation: true,
-		thirdPartyLicense: false,
-		config: {
-			resolve: {
-				alias: {
-					cookbook: resolve(__dirname, 'src'),
-					icons: resolve(
-						__dirname,
-						'node_modules/vue-material-design-icons'
-					),
+export default ({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return createAppConfig(
+		{
+			main: resolve(join('src', 'main.ts')),
+			guest: resolve(join('src', 'guest.ts')),
+		},
+		{
+			createEmptyCSSEntryPoints: true,
+			extractLicenseInformation: true,
+			thirdPartyLicense: false,
+			config: {
+				resolve: {
+					alias: {
+						cookbook: resolve(__dirname, 'src'),
+						icons: resolve(
+							__dirname,
+							'node_modules/vue-material-design-icons'
+						),
+					},
+				},
+				plugins: customPlugins,
+				server: {
+					port: Number(env.VITE_PORT ?? 5173),
+					strictPort: true,
 				},
 			},
-			plugins: customPlugins,
-		},
-	}
-)
+		}
+	)
+}
+```
