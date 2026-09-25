ad20e09491c5dee9cf28d932c7cb3d3cfb85b2f8
Christian Wolf <github@christianwolf.email>
2026-09-25 19:22:13 +0200

    Reverted some dropped compose config
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,9 +8,13 @@ declare(strict_types=1);
 use OCP\App\IAppManager;
 use OCP\Server;
 
+require_once __DIR__ . '/bootstrap_helper.php';
+
 define('PHPUNIT_RUN', 1);
 
 require_once __DIR__ . '/../../../lib/base.php';
 require_once __DIR__ . '/../../../tests/autoload.php';
+
+require_once __dir__ . '/../vendor/autoload.php';
 
 Server::get(IAppManager::class)->loadApp('cookbook');
