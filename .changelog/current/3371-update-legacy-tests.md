26b8d2e4b870332f1867e74ae360b103e5135a3b
Côme Chilliet <91878298+come-nc@users.noreply.github.com>
2026-09-25 19:47:28 +0200

    test: Remove legacy method calls from tests/bootstrap.php (#3371)
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>
    Signed-off-by: Christian Wolf <github@christianwolf.email>
    Co-authored-by: Christian Wolf <github@christianwolf.email>

---
+++ .changelog/current/3371-update-legacy-tests.md
@@ -0,0 +1,4 @@
+# Maintenance
+
+- Drop legacy code testing implementation and replace with current state of the art
+
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 // SPDX-FileCopyrightText: 2026 Nextcloud cookbook contributors
-//
 // SPDX-License-Identifier: AGPL-3.0-only OR AGPL-3.0-or-later
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 require_once __DIR__ . '/bootstrap_helper.php';
 
-require_once __DIR__ . '/../../../tests/bootstrap.php';
+define('PHPUNIT_RUN', 1);
+
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Fix for "Autoload path not allowed: .../cookbook/tests/testcase.php"
-\OC_App::loadApp('cookbook');
+Server::get(IAppManager::class)->loadApp('cookbook');
