b08a5f3f061049b2d21220b9be90993c7a08f29a
Christian Wolf <github@christianwolf.email>
2026-09-25 19:20:30 +0200

    build: enable hot module reload (#3378)
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@
 
 import { createAppConfig } from '@nextcloud/vite-config'
 import { join, resolve } from 'path'
-import { visualizer } from "rollup-plugin-visualizer"
+import { loadEnv } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 const customPlugins = []
 
@@ -17,25 +18,70 @@ if (process.env.ENABLE_BUNDLE_ANALYZER === 'true') {
 	}))
 }
 
+const devEntryRewritePlugin = {
+	name: 'cookbook-dev-entry-rewrite',
+	apply: 'serve',
+
+	configureServer(server) {
+		server.middlewares.use((req, res, next) => {
+			if (req.url?.startsWith('/apps-extra/cookbook/js/cookbook-main.mjs')) {
+				req.url = req.url.replace(
+					'/apps-extra/cookbook/js/cookbook-main.mjs',
+					'/apps-extra/cookbook/src/main.ts',
+				)
+			}
+
+			if (req.url?.startsWith('/apps-extra/cookbook/js/cookbook-guest.mjs')) {
+				req.url = req.url.replace(
+					'/apps-extra/cookbook/js/cookbook-guest.mjs',
+					'/apps-extra/cookbook/src/guest.ts',
+				)
+			}
+
+			next()
+		})
+	},
+}
+
 export default createAppConfig(
 	{
 		main: resolve(join('src', 'main.ts')),
 		guest: resolve(join('src', 'guest.ts')),
-	}, {
+	},
+	{
 		createEmptyCSSEntryPoints: true,
 		extractLicenseInformation: true,
 		thirdPartyLicense: false,
-		config: {
-			resolve: {
-				alias: {
-					cookbook: resolve(__dirname, 'src'),
-					icons: resolve(
-						__dirname,
-						'node_modules/vue-material-design-icons'
-					),
+		config: ({ mode, command }) => {
+			const env = loadEnv(mode, process.cwd(), '')
+			const devBase = '/apps-extra/cookbook/'
+
+			return {
+				base: command === 'serve' ? devBase : '/',
+				resolve: {
+					alias: {
+						cookbook: resolve(__dirname, 'src'),
+						icons: resolve(
+							__dirname,
+							'node_modules/vue-material-design-icons'
+						),
+					},
 				},
-			},
-			plugins: customPlugins,
+				plugins: [
+					...customPlugins,
+					devEntryRewritePlugin,
+				],
+				server: {
+					port: Number(env.VITE_PORT ?? 5173),
+					host: '0.0.0.0',
+					strictPort: true,
+					allowedHosts: [
+						'localhost',
+						'nextcloud.local',
+						'host.docker.internal',
+					],
+				},
+			}
 		},
 	}
 )
