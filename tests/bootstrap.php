ce1aa7d01e6f65af697b041cd8f83c00054b803b
Christian Wolf <github@christianwolf.email>
2026-09-25 19:24:17 +0200

    Update Changelog
    Signed-off-by: Christian Wolf <github@christianwolf.email>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,6 +15,6 @@ define('PHPUNIT_RUN', 1);
 require_once __DIR__ . '/../../../lib/base.php';
 require_once __DIR__ . '/../../../tests/autoload.php';
 
-require_once __dir__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 Server::get(IAppManager::class)->loadApp('cookbook');
